a05ae13d02bd13b08d6edc12d4bb4eba4b9ec765
Luke Marvelly <lmarvelly@hotmail.com>
2022-01-09 11:05:43 +0000

    Updated regular expression and code to stop users from deleting the date

--- src/components/RecordFormPage.js
+++ src/components/RecordFormPage.js
@@ -24,13 +24,17 @@ export default class RecordFormPage extends Component
 	onAmountChange = ( e ) => {
 		const amount = e.target.value;
 
-		if( amount.match(/^\d*(\.\d{0,2})?$/) )
+		// The amount is not able to be deleted if we do not include this OR statement. We also have the regular expression to prevent the wrong input being entered
+		if( !amount || amount.match(/^\d{1,}(\.\d{0,2})?$/) )
 		{
 			this.setState( () => ({ amount }) );
 		}
 	};
 	onDateChange = ( createdAt ) => {
-		this.setState( () => ({ createdAt }) )
+		if(createdAt) // prevents the user from deleting the date.
+		{
+			this.setState( () => ({ createdAt }) )
+		}
 	};
 	onFocusChange = ( { focused } ) => {
 		this.setState( () => ({ calenderFocused: focused }) );
