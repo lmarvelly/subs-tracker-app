82a81ab763b766c7f8bd8284e30f1b057dec1872
Luke Marvelly <lmarvelly@hotmail.com>
2022-01-11 13:02:49 +0000

    Updated Component to conect to redux and pass the state into RecordForm

--- src/components/EditRecordPage.js
+++ src/components/EditRecordPage.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import RecordForm from './RecordForm';
 
-const EditRecordPage = (props) => 
+const EditRecordPage = ( props ) => 
 {
-	console.log(props);
+	// <div>Edit Sub with ID: { props.match.params.id }</div>
+	// console.log(props);
 	return (
-		<div>Edit Sub with ID: { props.match.params.id }</div>
+		<RecordForm
+			record={ props.record }
+			onSubmit={ ( record ) => {
+				console.log( 'Updated', record );
+			}}
+		/>
 	)
 };
 
-export default EditRecordPage;
+// Give the component the current record object. We can take the props from the HOC and add to them
+const mapStateToProps = ( state, props ) =>
+{
+	return {
+		record: state.paymentRecord.find( ( record ) => record.id === props.match.params.id)
+	}
+};
+
+// The HOC passes the props through
+export default connect( mapStateToProps )( EditRecordPage );
