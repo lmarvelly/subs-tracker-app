bbcf8ff1ef4fb55b5864504fd34c69ad8b08da74
Luke Marvelly <lmarvelly@hotmail.com>
2022-01-18 14:06:43 +0000

    Added optional argument for adding Nickname to Member

--- src/actions/members.js
+++ src/actions/members.js
@@ -1,12 +1,13 @@
 import uuid from 'uuid';
 
-export const addMember = ( name ) => (
+export const addMember = ( name, nickName = '' ) => (
 console.log('ADD MEMBER'),
 {
 	type: 'ADD_MEMBER',
 	member:
 	{
 		name,
+		nickName,
 		playerUuid: uuid()
 	}
 });
