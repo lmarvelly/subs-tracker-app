01e3cd1f495d60e18129a927c50e39a13abd1188
Luke Marvelly <lmarvelly@hotmail.com>
2022-01-18 16:37:14 +0000

    Added Delete Member button

--- src/components/EditMemberPage.js
+++ src/components/EditMemberPage.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const EditMemberPage = () =>
+import { removeMember } from '../actions/members';
+
+const EditMemberPage = ( props ) =>
 {
+	const deleteButton = 
+	<button
+		onClick=
+		{
+			(e) =>
+			{
+				confirm('Are you sure you want to remove record?' ) &&
+				props.dispatch( removeMember( props.match.params.id ) );
+				props.history.push('/members'); // return to 
+			}
+		}
+	>
+		Delete
+	</button>
+
 	return (
-		<div></div>
+		<div>
+		<h2>Edit Member Page</h2>
+		{
+			deleteButton
+		}
+		</div>
 	);
 }
 
-export default EditMemberPage;
+const mapStateToProps = ( state, props ) =>
+{
+	return {
+		members: state.members
+	}
+};
+
+export default connect( mapStateToProps )( EditMemberPage );
