06c05846d1e0eedba9f09379b18ef8887c7d24b3
Luke Marvelly <lmarvelly@hotmail.com>
2022-02-01 14:06:56 +0000

    Refactored test data into seperate file

--- src/tests/selectors/record.test.js
+++ src/tests/selectors/record.test.js
@@ -1,40 +1,6 @@
 import moment from 'moment';
 import selectRecord from '../../selectors/records';
-
-const records = 
-[{
-	playerUuid: '83df2373-3a4e-4198-b428-a6728464bee1',
-	id: '1',
-	recordType: 'Debt',
-	description: 'Training subs',
-	note: '',
-	createdAt: 0,
-	amount: "",
-	amountOwed: 400,
-	amountPaid: 0
-},
-{
-	playerUuid: '83df2373-3a4e-4198-b428-a6728464bee1',
-	id: '2',
-	recordType: 'PAYMENT',
-	description: 'Training subs',
-	note: '',
-	createdAt: moment(0).subtract(4, 'days').valueOf(),
-	amount: "",
-	amountOwed: 400,
-	amountPaid: 0
-},
-{
-	playerUuid: '132ffb76-c4ea-4a98-b35f-58e0d090ea1e',
-	id: '3',
-	recordType: 'PAYMENT',
-	description: '5s subs',
-	note: '',
-	createdAt: moment(0).add(4, 'days').valueOf(),
-	amount: "",
-	amountOwed: 500,
-	amountPaid: 0
-}];
+import { records } from '../fixtures/fixures';
 
 test('Should filter by text value', () =>
 {
