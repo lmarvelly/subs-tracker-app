80be8d6fc5cf39d1a221954c0d7f62ef55027231
Luke Marvelly <lmarvelly@hotmail.com>
2022-02-01 09:40:04 +0000

    Refactoring

--- src/selectors/members.js
+++ src/selectors/members.js
@@ -3,11 +3,11 @@ export default ( members, { text, sortBy } ) =>
 	return members.filter( ( member )=>
 	{
 		const firstNameMatch = member.firstName.toLowerCase().includes(text.toLowerCase()); // ADD CODE FOR TEXT MATCH
-		const middleNames = member.nickName ? member.middleNames.toLowerCase().includes(text.toLowerCase()) : false; // ADD CODE FOR TEXT MATCH
+		const middleNames = member.nickname ? member.middleNames.toLowerCase().includes(text.toLowerCase()) : false; // ADD CODE FOR TEXT MATCH
 		const surname = member.surname.toLowerCase().includes(text.toLowerCase()); // ADD CODE FOR TEXT MATCH
-		const nickName = member.nickName ? member.nickName.toLowerCase().includes(text.toLowerCase()) : false; // ADD CODE FOR TEXT MATCH
+		const nickname = member.nickname ? member.nickname.toLowerCase().includes(text.toLowerCase()) : false; // ADD CODE FOR TEXT MATCH
 		
-		return firstNameMatch || middleNames || surname || nickName;
+		return firstNameMatch || middleNames || surname || nickname;
 	}).sort( (a, b) =>
 	{
 		if( sortBy === 'alphabetAsc' )
