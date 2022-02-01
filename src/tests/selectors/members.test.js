02ba2b221ddec4b40f085cddd5ade8c24a954db4
Luke Marvelly <lmarvelly@hotmail.com>
2022-02-01 14:25:25 +0000

    Added new tests for Members Selector

--- src/tests/selectors/members.test.js
+++ src/tests/selectors/members.test.js
@@ -1,7 +1,31 @@
 import selectMembers from '../../selectors/members';
 import { members } from '../fixtures/fixures';
 
-test('should filter by text value', () => 
+test('members should be ascending in alphabetical order', () => 
+{
+	const filters = 
+	{
+		sortBy: 'alphabetAsc'
+	}
+
+	const result = selectMembers( members, filters );
+
+	expect( result ).toEqual([members[1], members[2], members[0]]);
+});
+
+test('members should be in descending alphabetical order', () => 
+{
+	const filters = 
+	{
+		sortBy: 'alphabetDesc'
+	}
+
+	const result = selectMembers( members, filters );
+
+	expect( result ).toEqual([members[0], members[2], members[1]]);
+});
+
+test('should filter by text value and be in alphabetical order', () => 
 {
 	const filters = 
 	{
