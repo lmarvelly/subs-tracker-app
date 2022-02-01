954102111ed0af9ee91d83e094ad88890cf51f9c
Luke Marvelly <lmarvelly@hotmail.com>
2022-02-01 15:16:54 +0000

    Added tests for adding new Payment and new Debt

--- src/tests/reducers/records.test.js
+++ src/tests/reducers/records.test.js
@@ -1,7 +1,79 @@
 import recordReducer from '../../reducers/records';
+import { members, records } from '../fixtures/fixures';
 
-test('should set default state', () => {
+test('should set default state', () => 
+{
 	const state = recordReducer( undefined, { type: '@@INIT' } );
 
 	expect( state ).toEqual([]);
+});
+
+test('should remove a record using it\'s ID', () => 
+{
+	const id = records[1].id;
+	const action =
+	{
+		type: 'REMOVE_RECORD',
+		id
+	}
+	const state = recordReducer( records, action );
+
+	expect( state ).toEqual([ records[0], records[2] ]);
+});
+
+test('should not remove a record with ID that does not exist', () => 
+{
+	const id = 'abc';
+	const action =
+	{
+		type: 'REMOVE_RECORD',
+		id
+	}
+	const state = recordReducer( records, action );
+
+	expect( state ).toEqual( records );
+});
+
+test('should add a New Payment Record', () => 
+{
+	const newRecord = 
+	{ 
+		recordType: 'PAYMENT',
+		playerUuid: members[1].playerUuid, 
+		description: 'Training subs', 
+		amount: 400,
+		amountPaid: '',
+		amountOwed: '',
+		note: ''
+	}
+	const action = 
+	{
+		type: 'ADD_RECORD',
+		record: newRecord 
+	}
+	const state = recordReducer( records, action );
+
+	expect( state[3] ).toEqual(newRecord);
+});
+
+test('should add a New Debt Record', () => 
+{
+	const newRecord = 
+	{ 
+		recordType: 'DEBT',
+		playerUuid: members[1].playerUuid, 
+		description: '5\'s subs', 
+		amount: '',
+		amountPaid: 0,
+		amountOwed: 500,
+		note: ''
+	}
+	const action = 
+	{
+		type: 'ADD_RECORD',
+		record: newRecord 
+	}
+	const state = recordReducer( records, action );
+
+	expect( state[3] ).toEqual(newRecord);
 });
