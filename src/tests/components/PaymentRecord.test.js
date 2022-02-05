1941c96da722f70e19fc3f6cc1573cec241c9366
Luke Marvelly <lmarvelly@hotmail.com>
2022-02-05 12:57:54 +0000

    Finished tests for PaymentRecord component

--- src/tests/components/PaymentRecord.test.js
+++ src/tests/components/PaymentRecord.test.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Provider } from 'react-redux';
 
 import configureStore from '../../store/configureStore';
 // Importing the named export. To dynamically pass in props and make sure it's rendering correctly.
-import PaymentRecord from '../../components/PaymentRecord';
-import { records } from '../fixtures/fixures';
+import { PaymentRecord } from '../../components/PaymentRecord';
+import { records, members } from '../fixtures/fixures';
 
 const store = configureStore();
 
-test('should first render PaymentRecord with Payments/Debts', () => 
+test('should render PaymentRecord with Payments/Debts', () => 
 {
-	const wrapper = shallow( <Provider store={store} ><PaymentRecord paymentRecord={records} /></Provider> );
+	const wrapper = shallow( 
+		<PaymentRecord paymentRecord={records} members={members} />
+	);
+
+	expect(wrapper).toMatchSnapshot();
+});
+
+test('should render PaymentRecord with no Payments/Debts', () =>
+{
+  const wrapper = shallow(<PaymentRecord paymentRecord={[]} />);
+
+  expect(wrapper).toMatchSnapshot();
 });
