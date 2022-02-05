f1c9fce6476df93b3b58e35ef92479fdb2840134
Luke Marvelly <lmarvelly@hotmail.com>
2022-02-05 12:46:02 +0000

    Updated ID's

--- src/tests/fixtures/fixures.js
+++ src/tests/fixtures/fixures.js
@@ -3,8 +3,8 @@ import moment from 'moment';
 // Fixtures: In the test world, your fixture is the baseline test data
 export const records = 
 [{
-	playerUuid: '83df2373-3a4e-4198-b428-a6728464bee1',
-	id: '1',
+	playerUuid: 'player1',
+	id: 'record1',
 	recordType: 'DEBT',
 	description: 'Training subs',
 	note: '',
@@ -14,8 +14,8 @@ export const records =
 	amountPaid: 0
 },
 {
-	playerUuid: '83df2373-3a4e-4198-b428-a6728464bee1',
-	id: '2',
+	playerUuid: 'player2',
+	id: 'record2',
 	recordType: 'PAYMENT',
 	description: 'Training subs',
 	note: '',
@@ -25,8 +25,8 @@ export const records =
 	amountPaid: ''
 },
 {
-	playerUuid: '132ffb76-c4ea-4a98-b35f-58e0d090ea1e',
-	id: '3',
+	playerUuid: 'player3',
+	id: 'record3',
 	recordType: 'PAYMENT',
 	description: '5s subs',
 	note: '',
@@ -39,20 +39,20 @@ export const records =
 export const members = 
 [
 	{
-		playerUuid: '1',
+		playerUuid: 'player1',
 		firstName: 'Luke', 
 		middleNames: 'Owen Lloyd', 
 		surname: 'Marvelly', 
 		nickname: 'Lukio'
 	},
 	{
-		playerUuid: '2',
+		playerUuid: 'player2',
 		firstName: 'Harri', 
 		middleNames: '', 
 		surname: 'Messenger' 
 	},
 	{
-		playerUuid: '3',
+		playerUuid: 'player3',
 		firstName: 'Jason', 
 		middleNames: '', 
 		surname: 'Cousins'
