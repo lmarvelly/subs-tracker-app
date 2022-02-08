c7f753fe112ec6dca10d4f0b703a4f927660eb3e
Luke Marvelly <lmarvelly@hotmail.com>
2022-02-08 12:15:24 +0000

    Added default test case

--- src/tests/reducers/seasons.test.js
+++ src/tests/reducers/seasons.test.js
@@ -1,1 +1,10 @@
 import seasonReducer from '../../reducers/seasons';
+import { seasons } from '../fixtures/fixures';
+
+test('should set default state', () =>
+{
+	const state = seasonReducer( undefined, { type: '@@INIT' } );
+	console.log(state);
+
+	expect( state ).toEqual([]);
+});
