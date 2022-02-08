884ba487b6b3f268b369d89ab3af12043342f716
Luke Marvelly <lmarvelly@hotmail.com>
2022-02-08 11:55:10 +0000

    Created Reducer for Season functions and added case for 'ADD_SEASON'

---
+++ src/reducers/season.js
@@ -0,0 +1,18 @@
+const seasonReducerDefaultState = [];
+
+export default ( state = seasonReducerDefaultState, action ) =>
+{
+	switch ( action.type ) 
+	{
+		case 'ADD_SEASON':
+			return [
+				...state,
+				{
+					...action.season
+				}
+			]
+	
+		default:
+			break;
+	}
+}
