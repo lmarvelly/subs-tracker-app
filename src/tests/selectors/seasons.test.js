d0ee020b3fcf03576607d489ec69d4b340d54ba7
Luke Marvelly <lmarvelly@hotmail.com>
2022-02-08 18:56:58 +0000

    Created Tests for Season Filters

---
+++ src/tests/selectors/seasons.test.js
@@ -0,0 +1,36 @@
+import selectSeasons from '../../selectors/seasons';
+import { seasons } from '../fixtures/fixures';
+
+test('seasons should be ascending in alphabetical order', () => 
+{
+	const filters =
+	{
+		sortBy: 'ascending'
+	}
+	const result = selectSeasons( seasons, filters );
+
+	expect( result ).toEqual([ seasons[3], seasons[1], seasons[2], seasons[0] ]);
+});
+
+test('seasons should be descending in alphabetical order', () => 
+{
+	const filters =
+	{
+		sortBy: 'descending'
+	}
+	const result = selectSeasons( seasons, filters );
+
+	expect( result ).toEqual([ seasons[0], seasons[2], seasons[1], seasons[3] ]);
+});
+
+test('should filter by text value and be in descending order', () =>
+{
+	const filters =
+	{
+		sortBy: 'descending',
+		text: '20'
+	}
+	const result = selectSeasons( seasons, filters );
+
+	expect( result ).toEqual([ seasons[2], seasons[1], seasons[3] ]);
+});
