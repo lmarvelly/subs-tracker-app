564a2940ab8e7c2d04e64bee40e0d57535fda375
Luke Marvelly <lmarvelly@hotmail.com>
2022-02-08 18:50:27 +0000

    Added decending sorting

--- src/selectors/seasons.js
+++ src/selectors/seasons.js
@@ -14,6 +14,16 @@ export default ( seasons, { text = '', sortBy } ) =>
 
 			if ( nameA < nameB ) return -1;
 			if ( nameA > nameB ) return 1;
-		}
+			return 0;
+		};
+		if( sortBy === 'descending' )
+		{
+			const nameA = a.seasonName.toLowerCase();
+			const nameB = b.seasonName.toLowerCase();
+
+			if ( nameA > nameB ) return -1;
+			if ( nameA < nameB ) return 1;
+			return 0;
+		};
 	});
 }
