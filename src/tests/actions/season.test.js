74a4457d10518507516d7e799283e82643a7384d
Luke Marvelly <lmarvelly@hotmail.com>
2022-02-08 11:47:51 +0000

    Added test for addSeason function

---
+++ src/tests/actions/season.test.js
@@ -0,0 +1,16 @@
+import { addSeason } from '../../actions/season';
+
+test('should create member object', () => 
+{
+	const action = addSeason({ seasonName: '2021/2022' });
+
+	expect(action).toEqual(
+	{
+		type: 'ADD_SEASON',
+		"season":
+		{
+			seasonName: '2021/2022', 
+			seasonUuid: expect.any(String)
+		}
+	});
+});
