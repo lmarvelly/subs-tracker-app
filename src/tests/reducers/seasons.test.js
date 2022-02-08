ace1439b22de3dc67f0038bb123935fa4edc45c8
Luke Marvelly <lmarvelly@hotmail.com>
2022-02-08 12:29:53 +0000

    Added test for adding New Season

--- src/tests/reducers/seasons.test.js
+++ src/tests/reducers/seasons.test.js
@@ -4,7 +4,15 @@ import { seasons } from '../fixtures/fixures';
 test('should set default state', () =>
 {
 	const state = seasonReducer( undefined, { type: '@@INIT' } );
-	console.log(state);
 
 	expect( state ).toEqual([]);
+});
+
+test('should add a new season', () => 
+{
+	const season = { seasonName: '1999/2000' };
+	const action = { type: 'ADD_SEASON', season };
+	const state = seasonReducer( seasons, action );
+
+	expect( state ).toEqual([ ...seasons, season ]);
 });
