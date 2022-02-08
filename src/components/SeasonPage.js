4bec4a6fdd6d4287cbbc0d7bcfda05cb9ba10c79
Luke Marvelly <lmarvelly@hotmail.com>
2022-02-08 15:24:25 +0000

    Created SeasonPage component

---
+++ src/components/SeasonPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const SeasonPage = ( props ) =>
+{
+	return (
+		<div>
+			<h2>Season List</h2>
+		</div>
+	);
+}
+
+export default connect()(SeasonPage);
