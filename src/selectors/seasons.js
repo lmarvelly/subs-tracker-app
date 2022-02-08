b2172fc46eb01a038e96b7b6ba51c18d3c7adb87
Luke Marvelly <lmarvelly@hotmail.com>
2022-02-08 18:45:25 +0000

    Created selector for seasons

---
+++ src/selectors/seasons.js
@@ -0,0 +1,19 @@
+export default ( seasons, { text = '', sortBy } ) =>
+{
+	return seasons.filter( ( season ) => 
+	{
+		const seasonNameMatch = season.seasonName.toLowerCase().includes( text.toLowerCase() );
+
+		return seasonNameMatch;
+	}).sort( (a, b) =>
+	{
+		if( sortBy === 'ascending' )
+		{
+			const nameA = a.seasonName.toLowerCase();
+			const nameB = b.seasonName.toLowerCase();
+
+			if ( nameA < nameB ) return -1;
+			if ( nameA > nameB ) return 1;
+		}
+	});
+}
