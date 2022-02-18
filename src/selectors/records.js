da6b5006ac94c18668a5655fad90e3f77ae44f2c
Luke Marvelly <lmarvelly@hotmail.com>
2022-02-18 12:41:28 +0000

    Added Season Match filtering to Selector

--- src/selectors/records.js
+++ src/selectors/records.js
@@ -13,7 +13,7 @@ import moment from "moment";
  * @param {*} records 
  * @param {*} filters
  */
-export default ( records, members, { text, memberTextFilter, sortBy = 'dateAscending', startDate, endDate } ) =>
+export default ( records, members, { text, memberTextFilter, sortBy = 'dateAscending', startDate, endDate, seasonFilter } ) =>
 {
 	return records.filter( (record) =>
 	{
@@ -28,7 +28,9 @@ export default ( records, members, { text, memberTextFilter, sortBy = 'dateAscen
 		const endDateMatch  = endDate ? endDate.isSameOrAfter( createdAtMoment, 'day' ) : true ; // if the record is created the same day or after the endDate then it's filtered out
 		const textMatch = record.description.toLowerCase().includes(text.toLowerCase());
 		
-		return idMatch && startDateMatch && endDateMatch && textMatch; // Return true only if all the above are true. Record is removed if false
+		const seasonMatch = seasonFilter ? seasonFilter === record.seasonUuid : true;
+		
+		return idMatch && startDateMatch && endDateMatch && textMatch && seasonMatch; // Return true only if all the above are true. Record is removed if false
 	}).sort( (a, b) => 
 	{
 		if( sortBy === 'dateAscending' ) 
