4c2ed406149099da0db70e68b1997ec54b602e7f
Luke Marvelly <lmarvelly@hotmail.com>
2022-02-19 16:26:04 +0000

    Added test for description input

--- src/tests/components/RecordForm.test.js
+++ src/tests/components/RecordForm.test.js
@@ -22,7 +22,28 @@ test('should render Record Form with Record, member and season data', () =>
 	expect(wrapper).toMatchSnapshot();
 });
 
-// test('should render error for invalid form submission', () => 
-// {
-// 	const wrapper = shallow;
-// });
+test('should render error for invalid form submission', () => 
+{
+	const wrapper = shallow(<RecordForm members={[]} seasons={[]} />);
+	expect(wrapper).toMatchSnapshot();
+	wrapper.find('form').simulate('submit', 
+	{
+		preventDefault: () => {}
+	});
+	expect( wrapper.state('error').length ).toBeGreaterThan(0);
+	expect(wrapper).toMatchSnapshot();
+});
+
+test('should test description on input change', () => 
+{
+	const value = 'New description';
+	const wrapper = shallow(<RecordForm members={[]} seasons={[]} />);
+	wrapper.find('input').at(0).simulate('change',
+	{
+		target: { value } // Setting the value of e.target.value
+	});
+
+	expect(wrapper.state('description')).toBe(value);
+});
+
+// Add tests for all the rest of the inputs/dropdowns
