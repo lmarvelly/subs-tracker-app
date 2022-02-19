425d6117b9befb8ed60d5193252fa4bea2e2b489
Luke Marvelly <lmarvelly@hotmail.com>
2022-02-19 12:25:20 +0000

    Test to render Record Form with Record, member and season data

--- src/tests/components/RecordForm.test.js
+++ src/tests/components/RecordForm.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import RecordForm from '../../components/RecordForm';
-import { members, seasons } from '../fixtures/fixures';
+import { records, members, seasons } from '../fixtures/fixures';
 
 // TODO render without members or seasons array
 test('should render Record Form component correctly', () => 
@@ -13,5 +13,11 @@ test('should render Record Form component correctly', () =>
 test('should render Record Form with member and season data', () => 
 {
 	const wrapper = shallow(<RecordForm members={ members } seasons={ seasons } />);
+	expect(wrapper).toMatchSnapshot();
+});
+
+test('should render Record Form with Record, member and season data', () =>
+{
+	const wrapper = shallow(<RecordForm record={records[0]} members={ members } seasons={ seasons } />);
 	expect(wrapper).toMatchSnapshot();
 });
