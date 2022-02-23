81fac41ac0b810f2af1e936135af7b3b0b2d5ca8
Luke Marvelly <lmarvelly@hotmail.com>
2022-02-23 15:58:10 +0000

    Refactoring into Class

--- src/components/AddSeasonPage.js
+++ src/components/AddSeasonPage.js
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import SeasonForm from './SeasonForm';
 import { addSeason } from '../actions/seasons';
 
-const AddSeasonPage = ( props ) => (
-	<div>
-		<h1>Add Season Page</h1>
+export class AddSeasonPage extends Component
+{
+	constructor( props )
+	{
+		super( props );
+	}
 
-		<SeasonForm
-			onSubmit=
-			{
-				( season ) =>
-				{
-					props.dispatch( addSeason({ ...season }));
-					props.history.push('/seasons');
-				}
-			}
-		/>
-	</div>
-);
+	render()
+	{
+		return(
+			<div>
+				<h1>Add Season Page</h1>
 
-export default connect()( AddSeasonPage );
+				<SeasonForm
+					onSubmit=
+					{
+						( season ) =>
+						{
+							this.props.addSeason({ ...season });
+							this.props.history.push('/seasons');
+						}
+					}
+				/>
+			</div>
+		);
+	}
+}
+
+// Using mapDispatchToProps to make testing easier
+const mapDispatchToProps = ( dispatch ) => (
+{
+	addSeason: ({ ...season }) => dispatch( addSeason({ ...season }) )
+});
+
+export default connect( undefined, mapDispatchToProps )( AddSeasonPage );
