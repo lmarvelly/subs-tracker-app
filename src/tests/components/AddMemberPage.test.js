65c4609f25d892585641559cb59cbf97fe93f0a3
Luke Marvelly <lmarvelly@hotmail.com>
2022-02-23 15:39:46 +0000

    Added tests for Add Member Page

---
+++ src/tests/components/AddMemberPage.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { AddMemberPage } from '../../components/AddMemberPage';
+
+let addMember, member, history, wrapper;
+
+beforeEach( () => 
+{
+	member = 
+	{
+		playerUuid: 'player4',
+		firstName: 'John', 
+		middleNames: 'Philip', 
+		surname: 'Hobson', 
+		nickname: 'Johnson'
+	};
+	addMember = jest.fn();
+	history = { push: jest.fn() };
+	wrapper = shallow(
+		<AddMemberPage addMember={addMember} history={history} />
+	);
+});
+
+test('should render Add Member Page', () => {
+	expect(wrapper).toMatchSnapshot();
+});
+
