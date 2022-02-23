60a78df8b24412a91fb57fe0a697e1e7c0ba1710
Luke Marvelly <lmarvelly@hotmail.com>
2022-02-23 15:13:08 +0000

    Refactored into Class

--- src/components/AddMemberPage.js
+++ src/components/AddMemberPage.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import MemberForm from './MemberForm';
 import { addMember } from '../actions/members';
 
-const AddMemberPage = ( props ) => (
-	<div>
-		<h1>Add Member Page</h1>
+export class AddMemberPage extends Component
+{
+	constructor( props )
+	{
+		super( props );
+	}
 
-		<MemberForm
-			onSubmit={ ( member ) => {
-				props.dispatch( addMember( {...member} ) );
-				props.history.push('/members'); // Return to members page
-			}}
-		/>
-	</div>
-);
+	onSubmit =  ( member ) => 
+	{
+		this.props.dispatch( addMember({ ...member }) );
+		this.props.history.push('/members'); // Return to members page
+	}
+
+	render()
+	{
+		return (
+			<div>
+				<h1>Add Member Page</h1>
+
+				<MemberForm
+					onSubmit={ this.onSubmit }
+				/>
+			</div>
+		);
+	};
+};
 
 export default connect()( AddMemberPage );
