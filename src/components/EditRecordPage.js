f0a5f918cae98500041089f3819d96667fc556e3
Luke Marvelly <lmarvelly@hotmail.com>
2022-02-23 10:09:13 +0000

    Refactoring into Class based component

--- src/components/EditRecordPage.js
+++ src/components/EditRecordPage.js
@@ -1,45 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import RecordForm from './RecordForm';
 import { editRecord, removeRecord } from '../actions/records';
 
-const EditRecordPage = ( props ) => 
+/**
+ * Use classes to avoid inline functions. This avoids rerendering
+ * on every render.
+ * We put an export in front of the class keyword so we can test
+ * the unconnected version.
+ */
+export class EditRecordPage extends Component
 {
-	const deleteButton = <button onClick = 
+	constructor( props )
+	{
+		super( props );
+	}
+
+	deleteButton = <button onClick = 
 	{
 		(e) =>
 		{
 			confirm('Are you sure you want to remove record?') &&
-			props.dispatch( removeRecord( { id: props.record.id } ) );
-			props.history.push('/'); // return to dashboard
+			this.props.removeRecord( { id: this.props.record.id } );
+			this.props.history.push('/'); // return to dashboard
 		}
 	}
 	>
 		Delete
 	</button>
 
-	return (
-		<div>
-			<RecordForm
-				record={ props.record }
-				members={ props.members } // TODO change to member rather than all the members
-				seasons={ props.seasons }
-				onSubmit={ ( record ) => {
-					props.dispatch(
-						editRecord( 
-							record.id,
-							record
-						)
-					)
-
-					props.history.push('/'); // return to dashboard
-				}}
-			/>
-			{deleteButton}
-		</div>
-	)
-};
+	render()
+	{
+		return (
+			<div>
+				<RecordForm
+					record={ this.props.record }
+					members={ this.props.members } // TODO change to member rather than all the members
+					seasons={ this.props.seasons }
+					onSubmit={ ( record ) => 
+					{
+						this.props.editRecord( record );
+	
+						this.props.history.push('/'); // return to dashboard
+					}}
+				/>
+				{ this.deleteButton }
+			</div>
+		)
+	}
+}
 
 // Give the component the current record object. We can take the props from the HOC and add to them
 // This goes out of sync atm if you refresh the page because a new id gets generated for the record
@@ -52,5 +62,11 @@ const mapStateToProps = ( state, props ) =>
 	}
 };
 
+const mapDispatchToProps = ( dispatch ) => (
+{
+	editRecord: ( record ) => dispatch( editRecord( record.id, record ) ),
+	removeRecord: ( { id } ) => dispatch( removeRecord( { id } ) )
+});
+
 // The HOC passes the props through
-export default connect( mapStateToProps )( EditRecordPage );
+export default connect( mapStateToProps, mapDispatchToProps )( EditRecordPage );
