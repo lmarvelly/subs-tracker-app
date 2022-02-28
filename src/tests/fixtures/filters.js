6e1d3a693fc9eeb7693cf56fcd573dbd46594767
Luke Marvelly <lmarvelly@hotmail.com>
2022-02-28 10:49:20 +0000

    Created fake filters for testing

---
+++ src/tests/fixtures/filters.js
@@ -0,0 +1,53 @@
+import moment from "moment";
+
+import { seasons } from "./fixures";
+
+const defaultFilters =
+{
+	// descriptionTextFilter: '',
+	// memberTextFilter: '',
+	// sortBy: 'dateAscending',
+	// startDate: undefined,
+	// endDate: undefined,
+	// seasonFilter: ''
+
+	descriptionTextFilter: '',
+	memberTextFilter: '',
+	playerUuid: '',
+	sortBy: 'dateAscending',
+	startDate: moment().startOf('month'),
+	endDate: moment().endOf('month'),
+	seasonFilter: ''
+}
+
+const altFilters1 =
+{
+	descriptionTextFilter: '5s',
+	memberTextFilter: '',
+	sortBy: 'dateDescending',
+	startDate: moment(),
+	endDate: moment().add( 3, 'days' ),
+	seasonFilter: ''
+}
+
+const altFilters2 =
+{
+	descriptionTextFilter: '',
+	memberTextFilter: 'Harri',
+	sortBy: 'dateDescending',
+	startDate: moment(),
+	endDate: moment().add( 3, 'days' ),
+	seasonFilter: ''
+}
+
+const altFilters3 =
+{
+	descriptionTextFilter: '',
+	memberTextFilter: 'Harri',
+	sortBy: 'dateDescending',
+	startDate: moment(),
+	endDate: moment().add( 3, 'days' ),
+	seasonFilter: seasons[0].seasonUuid
+}
+
+export { defaultFilters, altFilters1, altFilters2, altFilters3 };
