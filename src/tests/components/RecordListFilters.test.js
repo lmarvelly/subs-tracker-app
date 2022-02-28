ce4c10c186d58fad3b9c39436322deafef6ffdef
Luke Marvelly <lmarvelly@hotmail.com>
2022-02-28 13:45:39 +0000

    Fixed mock function errors

--- src/tests/components/RecordListFilters.test.js
+++ src/tests/components/RecordListFilters.test.js
@@ -4,34 +4,61 @@ import { RecordListFilters } from '../../components/RecordListFilters';
 import { members, seasons } from '../fixtures/fixures';
 import { defaultFilters, altFilters1, altFilters2, altFilters3 } from '../fixtures/filters';
 
-let descriptionTextFilter, memberTextFilter, sortBy, startDate,
-endDate, seasonFilter, wrapper;
+let setDescriptionTextFilter, setMemberFilterText, 
+sortByDateAscending, sortByDateDescending, setStartDate, setEndDate,
+setSeasonFilter, emptyWrapper, wrapper;
 
 beforeEach( () =>
 {
-	descriptionTextFilter = jest.fn();
-	memberTextFilter = jest.fn();
-	sortBy = jest.fn();
-	startDate = jest.fn();
-	endDate = jest.fn();
-	seasonFilter = jest.fn();
-	wrapper = shallow( 
+	setDescriptionTextFilter = jest.fn();
+	setMemberFilterText = jest.fn();
+	sortByDateAscending = jest.fn();
+	sortByDateDescending = jest.fn();
+	setStartDate = jest.fn();
+	setEndDate = jest.fn();
+	setSeasonFilter = jest.fn();
+	
+	emptyWrapper = shallow( 
 		<RecordListFilters 
-			descriptionTextFilter={descriptionTextFilter}
-			memberTextFilter={memberTextFilter}
-			sortBy={sortBy}
-			startDate={startDate}
-			endDate={endDate}
-			seasonFilter={seasonFilter}
+			setDescriptionTextFilter={setDescriptionTextFilter}
+			setMemberFilterText={setMemberFilterText}
+			sortByDateAscending={sortByDateAscending}
+			sortByDateDescending={sortByDateDescending}
+			setStartDate={setStartDate}
+			setEndDate={setEndDate}
+			setSeasonFilter={setSeasonFilter}
+
 			recordFilters={defaultFilters}
 
 			members={[]}
 			seasons={[]}
 		/> 
-		);
+	);
+
+	wrapper = shallow( 
+		<RecordListFilters 
+			setDescriptionTextFilter={setDescriptionTextFilter}
+			setMemberFilterText={setMemberFilterText}
+			sortByDateAscending={sortByDateAscending}
+			sortByDateDescending={sortByDateDescending}
+			setStartDate={setStartDate}
+			setEndDate={setEndDate}
+			setSeasonFilter={setSeasonFilter}
+
+			recordFilters={defaultFilters}
+
+			members={members}
+			seasons={seasons}
+		/> 
+	);
 });
 
-test('should Render RecordListFilters correctly', () => 
+test('should Render RecordListFilters correctly with no data', () => 
+{
+	expect(emptyWrapper).toMatchSnapshot();
+});
+
+test('should Render RecordListFilters correctly with members and season data', () => 
 {
 	expect(wrapper).toMatchSnapshot();
 });
@@ -46,7 +73,6 @@ test('should Render RecordListFilters with Alt Data 1 correctly', () =>
 	expect(wrapper).toMatchSnapshot();
 });
 
-// Error: memberTextFilter value prop missing
 test('should Render RecordListFilters with Alt Data 2 correctly', () => 
 {
 	wrapper.setProps(
@@ -57,7 +83,6 @@ test('should Render RecordListFilters with Alt Data 2 correctly', () =>
 	expect(wrapper).toMatchSnapshot();
 });
 
-// Error: memberTextFilter value prop missing
 test('should Render RecordListFilters with Alt Data 3 correctly', () => 
 {
 	wrapper.setProps(
@@ -66,4 +91,31 @@ test('should Render RecordListFilters with Alt Data 3 correctly', () =>
 	});
 
 	expect(wrapper).toMatchSnapshot();
+});
+
+// Failing. this.props.setMemberFilterText is not a function
+test('should handle text change on Member Filter', () =>
+{
+	const value = 'Luke';
+	console.log(wrapper.find('input').at(0).type());
+	wrapper.find('input').at(0).simulate('change', 
+	{
+		target: { value }
+	});
+
+	expect(wrapper.find('input').at(0).value).toBe(value);
+});
+
+test('should sortBy date', () => 
+{
+	const value = 'dateDescending';
+	const select = wrapper.find('select').at(0);
+	select.simulate('change',
+	{
+		target: { value }
+	});
+
+	console.log('State:', wrapper.state);
+
+	expect(wrapper.state('sortBy')).toBe(sortBy);
 });
