7da75ee35681c1013efa317540b47875495f5607
Luke Marvelly <lmarvelly@hotmail.com>
2022-03-01 15:41:29 +0000

    Added Record Totals component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PaymentRecord from './PaymentRecord';
 import RecordListFilters from './RecordListFilters';
+import RecordTotals from './RecordTotals';
 
 const Dashboard = () => (
 	<div>
+		<RecordTotals />
 		<RecordListFilters />
 		<PaymentRecord />
 	</div>
