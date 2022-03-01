22348774e8c5567bcf4dd4e49831b3e0cc858d52
Luke Marvelly <lmarvelly@hotmail.com>
2022-03-01 17:59:16 +0000

    Created a selector to grab the Total Income and Debts from the records

---
+++ src/selectors/record-totals.js
@@ -0,0 +1,22 @@
+export default ( records ) =>
+{
+	let totalIncome = 0;
+	let totalDebt = 0;
+	records.map( (record) =>
+	{
+		if ( typeof record.amount === 'number' ) // if amount is a number
+		{
+			totalIncome += record.amount;
+		}
+		if ( typeof record.amountPaid === 'number' ) // if amount is a number
+		{
+			totalIncome += record.amountPaid;
+		}
+		if ( typeof record.amountOwed === 'number' )
+		{
+			totalDebt += record.amountOwed;
+		}
+	});
+	
+	return { totalIncome, totalDebt };
+}
