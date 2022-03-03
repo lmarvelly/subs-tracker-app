b989218e38d7561f8fab86c593d0aa082b458cb8
Luke Marvelly <lmarvelly@hotmail.com>
2022-03-03 11:29:18 +0000

    Fixed test rendering RecordSummary with data

--- src/tests/components/RecordSummary.test.js
+++ src/tests/components/RecordSummary.test.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-// import selectRecords from '../../selectors/records';
-import RecordSummary from '../../components/RecordSummary';
+import { RecordSummary } from '../../components/RecordSummary';
 // Importing the named export. To dynamically pass in props and make sure it's rendering correctly.
-import { PaymentRecord } from '../../components/PaymentRecord';
 import { records, members, seasons } from '../fixtures/fixures';
+import { defaultFilters } from '../fixtures/filters';
+import recordTotals from '../../selectors/record-totals';
 
 test('should render Record Totals correctly without data', () => 
 { 
-	// const wrapper = shallow(<RecordTotals paymentRecord={[]} />);
+	const wrapper = shallow(<RecordSummary />);
 
-	// expect(wrapper).toMatchSnapshot();
+	expect(wrapper).toMatchSnapshot();
+});
+
+test('should render Record Totals correctly with record data', () => 
+{
+	const totals = recordTotals( records );
+	const length = records.length;
+	const wrapper = shallow(
+		<RecordSummary 
+			recordLength={ length } 
+			recordTotals=
+			{{
+				totalIncome: totals.totalIncome,
+				totalDebt: totals.totalDebt
+			}}
+		/>
+	);
+
+	expect(wrapper).toMatchSnapshot();
 });
