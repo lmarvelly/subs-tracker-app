9972b4ca05d7ecdb73976809cd680bf41503ff2a
Luke Marvelly <lmarvelly@hotmail.com>
2022-03-03 13:23:01 +0000

    Added handling for Season Filters

--- src/components/RecordSummary.js
+++ src/components/RecordSummary.js
@@ -1,16 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import numeral from 'numeral';
 
 import selectRecords from '../selectors/records';
 import recordTotals from '../selectors/record-totals';
 
-export const RecordSummary = ({ recordLength = 0, recordTotals = { totalIncome: 0, totaldebt: 0 } }) =>
+// Example for defensive programming: Setting props coming from mapStateToProps to default values
+export const RecordSummary = ({ recordLength = 0, recordTotals = { totalIncome: 0, totaldebt: 0 }, seasons = [], seasonFilter = '' }) =>
 {
 	const recordWord = recordLength === 1 ? 'record' : 'records';
+
+	const seasonWording = () =>
+	{
+		const season = seasons.find( ( season ) => season.seasonUuid === seasonFilter );
+
+		if( seasonFilter )
+		{
+			return `from ${season.seasonName}`;
+		}
+		else 
+		{
+			return `${seasons.length} seasons`;
+		}
+	};
+
 	return(
 		<div>
-			<h1>{`Viewing: ${recordLength} ${recordWord}`}</h1>
+			<h1>{`Viewing: ${recordLength} ${recordWord} ${seasonWording()}`}</h1>
 			<h1>{`Total income: £${numeral(recordTotals.totalIncome / 100).format('0,0.00')}`}</h1>
 			<h1>{`Total debt: £${numeral(recordTotals.totalDebt / 100).format('0,0.00')}`}</h1>
 		</div>
@@ -22,7 +38,9 @@ const mapStateToProps = ( state ) =>
 	const paymentRecord = selectRecords(state.paymentRecord, state.members, state.recordFilters)
 	return{
 		recordLength: paymentRecord.length,
-		recordTotals: recordTotals(paymentRecord)
+		recordTotals: recordTotals(paymentRecord),
+		seasons: state.seasons,
+		seasonFilter: state.recordFilters.seasonFilter ? state.recordFilters.seasonFilter : ''
 	}
 }
 
