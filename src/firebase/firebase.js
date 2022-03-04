c027607c7167e383d40627fd9de7c59312600c7e
Luke Marvelly <lmarvelly@hotmail.com>
2022-03-04 13:36:00 +0000

    Created Firebase config file with a simple call to make sure it's working

---
+++ src/firebase/firebase.js
@@ -0,0 +1,41 @@
+// New Syntax:
+// Gets all the variables in 'firebase' and dumps them into the variable firebase
+import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = 
+{
+	apiKey: "AIzaSyBPoO2oWL1x-24g0lMaIpuImJ3ydSwMgrs",
+	authDomain: "subs-tracker-d6bfb.firebaseapp.com",
+	databaseURL: "https://subs-tracker-d6bfb-default-rtdb.europe-west1.firebasedatabase.app",
+	projectId: "subs-tracker-d6bfb",
+	storageBucket: "subs-tracker-d6bfb.appspot.com",
+	messagingSenderId: "598487712485",
+	appId: "1:598487712485:web:4b694627691d75d60e4a0c",
+	measurementId: "G-RR8L5DT0YM"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
+// ref is short for reference
+database.ref().set(
+{
+	name: "Luke Marvelly",
+	age: 34,
+	isSingle: true,
+	location: {
+		city: 'Cardiff',
+		country: 'Wales'
+	}
+});
+
+database.ref('age').set(37);
+database.ref('location/city').set('Aberystwyth');
+
+database.ref('attributes/').set(
+{
+	height: '5.10ft',
+	weight: '11.1lb'
+});
