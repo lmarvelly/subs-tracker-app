0908147edb1249eeba2d5ef2afed2cca6660efd5
Luke Marvelly <lmarvelly@hotmail.com>
2022-03-08 12:38:49 +0000

    Added function to handle adding members to firebase

--- src/actions/members.js
+++ src/actions/members.js
@@ -1,17 +1,39 @@
-import uuid from 'uuid';
+import database from '../firebase/firebase';
 
-export const addMember = ( { firstName = '', middleNames = '', surname = '', nickname = '' } ) => (
+export const addMember = ( member ) => (
 {
 	type: 'ADD_MEMBER',
-	member:
-	{
-		firstName,
-		middleNames,
-		surname,
-		nickname,
-		playerUuid: uuid()
-	}
+	member
 });
+
+export const startAddMember = ( memberData = {} ) =>
+{
+	return (dispatch) => 
+	{
+		const {
+			firstName,
+			middleNames,
+			surname,
+			nickname
+		} = memberData; // Deconstruct member data
+
+		const member = 
+		{
+			firstName, middleNames, surname, nickname
+		}
+
+		database.ref('subs-tracker/members')
+			.push(member)
+			.then((ref) =>
+			{
+				dispatch(addMember(
+				{
+					playerUuid: ref.key, // the key that firebase generates
+					...member
+				}));
+			});
+	};
+};
 
 export const editMember = ( playerUuid, updates ) => (
 {
