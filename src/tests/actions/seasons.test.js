bf9adb0f6f42b77802ad0d68144c73b25075ca62
Luke Marvelly <lmarvelly@hotmail.com>
2022-03-15 10:51:43 +0000

    Added test but is failing

--- src/tests/actions/seasons.test.js
+++ src/tests/actions/seasons.test.js
@@ -1,5 +1,19 @@
-import { addSeason, editSeason, removeSeason } from '../../actions/seasons';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { addSeason, editSeason, removeSeason, startAddSeason } from '../../actions/seasons';
 import { seasons } from '../fixtures/fixures';
+import database from '../../firebase/firebase';
+
+const createMockStore = configureMockStore([thunk]);
+
+beforeEach((done) =>
+{
+	// const 
+
+	database.ref('subs-tracker/seasons')
+		.set({})
+		.then(() => done());
+});
 
 test('should create a Add Season Action Object', () => 
 {
@@ -33,5 +47,37 @@ test('should create a Delete Season Action Object', () =>
 	{
 		type: 'REMOVE_SEASON',
 		seasonUuid: 'abc123'
+	});
+});
+
+test('should add a season to the database', (done) => 
+{ 
+	const store = createMockStore({});
+
+	const seasonData = { seasonName: '1999/2000' };
+
+	const promise = store.dispatch(startAddSeason(seasonData)).then(() =>
+	{
+		const actions = store.getActions();
+		expect(actions[0]).toEqual(
+		{
+			type: 'ADD_SEASON',
+			season:
+			{
+				seasonUuid: expect.any(String),
+				...seasonData
+			}
+		});
+
+		return database.ref(`subs-tracker/seasons/${actions[0].season.id}`).once('value');
+	});
+
+	promise.then((snapshot) =>
+	{
+		expect(snapshot.val()).toEqual(
+		{
+			...defaultSeason 
+		});
+		done();
 	});
 });
