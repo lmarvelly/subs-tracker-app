f405e42961a0081050930a75da9fe995bfb3ba2a
Luke Marvelly <lmarvelly@hotmail.com>
2022-03-27 15:39:15 +0100

    Added Logout function

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,8 +1,17 @@
 import { firebase, googleAuthProvider } from '../firebase/firebase';
 
+// Return a Promise from firebase
 export const startLogin = () =>
 {
 	return () => {
 		return firebase.auth().signInWithPopup(googleAuthProvider);
+	};
+};
+
+// Return a Promise from firebase
+export const startLogout = () =>
+{
+	return () => {
+		return firebase.auth().signOut();
 	};
 };
