4d33ada91c41e1f2dc4519e8f6639082c72b2c74
Luke Marvelly <lmarvelly@hotmail.com>
2022-03-27 16:11:15 +0100

    Added Login Button test

--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -2,8 +2,21 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { LoginPage } from '../../components/LoginPage';
 
-test('should render Login Page correctly', () => {
-	const wrapper = shallow(<LoginPage />);
+let wrapper, startLogin;
 
+beforeEach(() =>
+{
+	startLogin = jest.fn();
+
+	wrapper = shallow(<LoginPage startLogin={startLogin} />);
+})
+
+test('should render Login Page correctly', () => {
 	expect(wrapper).toMatchSnapshot();
+});
+
+test('should test Login button', () => {
+	wrapper.find('button').at(0).simulate('click');
+
+	expect(startLogin).toHaveBeenCalled();
 });
