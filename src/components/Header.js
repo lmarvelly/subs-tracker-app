ce1b8b110aae2015371de9464dc7b61e5d4c3050
Luke Marvelly <lmarvelly@hotmail.com>
2022-03-27 15:44:44 +0100

    Added named header for testing with Redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { startLogout } from '../actions/auth';
 
 /**
  * @class NavLink make use of client side routing so we don't have
  * fully page reloads.
+ * 
+ * @param startLogout is got from Dispatch Props
  */
- const Header = () => (
+ export const Header = ({ startLogout }) => (
 	<header>
 		<h1>Subs Tracker</h1>
 		<NavLink to='/' activeClassName='is-active' exact={true}>Home </NavLink>
@@ -15,7 +19,14 @@ import { NavLink } from 'react-router-dom';
 		<NavLink to='/members' activeClassName='is-active'>Members Page </NavLink>
 		<NavLink to='/seasons' activeClassName='is-active'>Seasons Page </NavLink>
 		<NavLink to='/help' activeClassName='is-active'>Help Page </NavLink>
+		<button onClick={startLogout}>Logout</button>
 	</header>
 );
 
-export default Header;
+// Return an object
+const mapDispatchToProps = (dispatch) => (
+{
+	startLogout: () => dispatch(startLogout())
+});
+
+export default connect(undefined,mapDispatchToProps)(Header);
