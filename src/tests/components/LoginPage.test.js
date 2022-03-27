a4138cc4537a4ea998bf0942dd15cf51bc412341
Luke Marvelly <lmarvelly@hotmail.com>
2022-03-27 15:38:36 +0100

    Fixing import error

--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import LoginPage from '../../components/LoginPage';
+import { LoginPage } from '../../components/LoginPage';
 
-test('should first', () => {
+test('should render Login Page correctly', () => {
 	const wrapper = shallow(<LoginPage />);
 
 	expect(wrapper).toMatchSnapshot();
