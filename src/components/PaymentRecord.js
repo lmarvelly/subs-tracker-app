66189e86c8b9d1008c4f7651ec9017e79c7e047d
Luke Marvelly <lmarvelly@hotmail.com>
2022-04-04 17:56:23 +0100

    Creating onSubmit functon, refactoring and passing it down to RecordListItem component

--- src/components/PaymentRecord.js
+++ src/components/PaymentRecord.js
@@ -1,33 +1,46 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { startEditRecord } from '../actions/records';
 import selectRecords from '../selectors/records';
 import RecordListItem from './RecordListItem';
 
-export const PaymentRecord = (props) => (
-	<div>
-		<h1>Payment Record</h1>
-		{
-			props.paymentRecord.length === 0 ? ( <p>No Payments</p> )
-			:
-			props.paymentRecord.map(( record ) =>
-			{
-				const member = props.members.find( (member) => record.playerUuid === member.playerUuid )
-				const season = props.seasons.find( (season) => record.seasonUuid === season.seasonUuid )
-				const name = member.nickname ? `${member.firstName} '${member.nickname}' ${member.surname}` : `${member.firstName} ${member.surname}`
+// Write onSubmit function to be passed down to RecordListItem
 
-				return (
-					<RecordListItem 
-						key={record.id} 
-						name={name} 
-						seasonName={season.seasonName}
-						{...record} 
-					/>
-				)
-			})
-		}
-	</div>
-);
+export const PaymentRecord = (props) => 
+{
+	const onSubmit = ( record ) =>
+	{
+		props.startEditRecord( record );
+	};
+
+	return (
+		<div>
+			<h1>Payment Record</h1>
+			{
+				props.paymentRecord.length === 0 ? ( <p>No Payments</p> )
+				:
+				props.paymentRecord.map(( record ) =>
+				{
+					const member = props.members.find( (member) => record.playerUuid === member.playerUuid )
+					const season = props.seasons.find( (season) => record.seasonUuid === season.seasonUuid )
+					const name = member.nickname ? `${member.firstName} '${member.nickname}' ${member.surname}` : `${member.firstName} ${member.surname}`
+
+					return (
+						<RecordListItem 
+							key={record.id} 
+							name={name} 
+							record={record}
+							seasonName={season.seasonName}
+							{...record} 
+							onSubmit={onSubmit}
+						/>
+					)
+				})
+			}
+		</div>
+	);
+};
 
 /**
  * A simple HOC function that takes in a component as an 
@@ -46,10 +59,15 @@ const mapStateToProps = (state) =>
 	}
 };
 
+const mapDispatchToProps = ( dispatch, props ) => (
+{
+	startEditRecord: ( record ) => dispatch( startEditRecord( record.id, record.recordType, record ) )
+});
+
 /**
  * @function connect() This is a HOC (A higher Order Component)
  * connects to the state of the app. The function references
  * inside, mapStateToProps() determines what info we want this 
  * component to access.
  */
-export default connect( mapStateToProps )( PaymentRecord );
+export default connect( mapStateToProps, mapDispatchToProps )( PaymentRecord );
