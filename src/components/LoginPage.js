0d0c8d1ea5286da45202a5a07e43a61db90d78e2
Luke Marvelly <lmarvelly@hotmail.com>
2022-04-04 12:32:40 +0100

    Adding styles

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,9 +3,12 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 export const LoginPage = ({ startLogin }) => (
-	<div>
-		<h1>Login Page</h1>
-		<button onClick={startLogin}>Login</button>
+	<div className='box-layout'>
+		<div className='box-layout__box'>
+			<h1 className='box-layout__title'>Subs Tracker App</h1>
+			<p>It's time to get your teams subs under control.</p>
+			<button onClick={startLogin}>Login</button>
+		</div>
 	</div>
 );
 
