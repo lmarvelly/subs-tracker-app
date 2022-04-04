c339b050dd2dfa4523d010ffdcd983bf15e4d8a4
Luke Marvelly <lmarvelly@hotmail.com>
2022-04-04 15:59:18 +0100

    Refactored into Class component and added some functionality to handle Dept Payment

--- src/components/RecordListItem.js
+++ src/components/RecordListItem.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
 import numeral from 'numeral';
+
+import { startEditRecord } from '../actions/records';
 
 /**
  * @param {*} props Is deconstructed into
@@ -14,37 +17,74 @@ import numeral from 'numeral';
  * 
  * No need to define mapStateToProps because everything we need,
  * including dispatch, is being passed down from PaymentRecord 
- * 
- * @returns 
  */
-const RecordListItem = ( {name, dispatch, id, playerUuid, seasonName, amount, amountOwed, amountPaid, recordType, description, createdAt }) =>
+class RecordListItem extends Component
 {
-	const debtItem = (
-		<div>
-			<p>Debt amount: { `£${numeral(amountOwed / 100).format('0,0.00')}` }</p>
-			£<input 
-				type="text" 
-				value={amountPaid / 100} 
-			/>
-		</div>
-	);
+	constructor( props )
+	{
+		super( props );
 
-	return (
-		<div>
-			<Link to={`/edit-record/${id}`}>
-				<h2>Description: { description }</h2>
-			</Link>
-			<h3>Player: { name }</h3>
-			<p>Season: { seasonName }</p>
+		this.state =
+		{
+			amountPaid: this.props.amountPaid / 100,
+			error: ''
+		}
+
+		this.onAmountChange = this.onAmountChange.bind( this );
+	}
+
+	onAmountChange( e )
+	{
+		const amount = e.target.value;
+
+		// The amount is not able to be deleted if we do not include this OR statement. We also have the regular expression to prevent the wrong input being entered
+		if( !amount || amount.match(/^\d{1,}(\.\d{0,2})?$/) )
+		{
+			if( amount <= (this.props.amountOwed / 100 ))
 			{
-				recordType === 'PAYMENT' && <p>Payment Amount: { `£${numeral(amount / 100).format('0,0.00')}` }</p>
+				this.setState( () => ({amountPaid: amount}) );
 			}
+			else
 			{
-				recordType === 'DEBT' && debtItem
+				alert('Cannot do that')
 			}
-			<p>Created At: { moment( createdAt).format( "DD-MM-YYYY") }</p>
-		</div>
-	);
+		}
+	};
+
+	render()
+	{
+		const debtItem = (
+			<div>
+				<p>Debt amount: { `£${numeral(this.props.amountOwed / 100).format('0,0.00')}` }</p>
+				£<input 
+					type="text" 
+					value={this.state.amountPaid}
+					onChange={this.onAmountChange}
+				/>
+			</div>
+		);
+		return(
+			<div>
+				<Link to={`/edit-record/${this.props.id}`}>
+					<h2>Description: { this.props.description }</h2>
+				</Link>
+				<h3>Player: { this.props.name }</h3>
+				<p>Season: { this.props.seasonName }</p>
+				{
+					this.props.recordType === 'PAYMENT' && <p>Payment Amount: { `£${numeral(this.props.amount / 100).format('0,0.00')}` }</p>
+				}
+				{
+					this.props.recordType === 'DEBT' && debtItem
+				}
+				<p>Created At: { moment( this.props.createdAt).format( "DD-MM-YYYY") }</p>
+			</div>
+		);
+	}
 }
 
-export default RecordListItem;
+const mapDispatchToProps = ( dispatch, props ) => (
+{
+	startEditRecord: ( record ) => dispatch( startEditRecord(record.id, record.recordType, record) )
+});
+
+export default connect( undefined, mapDispatchToProps )( RecordListItem );
