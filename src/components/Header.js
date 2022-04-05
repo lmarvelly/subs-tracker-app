19d5e92aa3e6593bc3182073cd656667f25a7499
Luke Marvelly <lmarvelly@hotmail.com>
2022-04-05 10:16:24 +0100

    Refactoring using Link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { startLogout } from '../actions/auth';
 
 /**
- * @class NavLink make use of client side routing so we don't have
+ * @class Link make use of client side routing so we don't have
  * fully page reloads.
  * 
  * @param startLogout is got from Dispatch Props
  */
  export const Header = ({ startLogout }) => (
 	<header>
-		<h1>Subs Tracker</h1>
-		<NavLink to='/dashboard' activeClassName='is-active'>Home </NavLink>
-		<NavLink to='/add-record' activeClassName='is-active'>Add Record </NavLink>
-		<NavLink to='/add-member' activeClassName='is-active'>Add Member </NavLink>
-		<NavLink to='/add-season' activeClassName='is-active'>Add Season </NavLink>
-		<NavLink to='/members' activeClassName='is-active'>Members Page </NavLink>
-		<NavLink to='/seasons' activeClassName='is-active'>Seasons Page </NavLink>
-		<NavLink to='/help' activeClassName='is-active'>Help Page </NavLink>
+		<Link to='/dashboard' activeClassName='is-active'>
+			<h1>Subs Tracker</h1>
+		</Link>
+		<Link to='/add-record' activeClassName='is-active'>Add Record </Link>
+		<Link to='/add-member' activeClassName='is-active'>Add Member </Link>
+		<Link to='/add-season' activeClassName='is-active'>Add Season </Link>
+		<Link to='/members' activeClassName='is-active'>Members Page </Link>
+		<Link to='/seasons' activeClassName='is-active'>Seasons Page </Link>
+		<Link to='/help' activeClassName='is-active'>Help Page </Link>
 		<button onClick={startLogout}>Logout</button>
 	</header>
 );
