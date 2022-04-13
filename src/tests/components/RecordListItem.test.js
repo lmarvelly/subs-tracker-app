21563a80b673e4e63db80eda5197150ebd37c8ad
Luke Marvelly <lmarvelly@hotmail.com>
2022-04-13 17:04:01 +0100

    Adding new tests for Compressed/Uncompressed RecordListItems

--- src/tests/components/RecordListItem.test.js
+++ src/tests/components/RecordListItem.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import RecordListItem from '../../components/RecordListItem';
 import { records, members, seasons } from '../fixtures/fixures';
 
-test('should render one RecordListItem', () => 
+test('should render one compressed RecordListItem payment', () => 
 {
 	const record = records[1];
 	const member = members.find( (member) => record.playerUuid === member.playerUuid );
@@ -19,5 +19,54 @@ test('should render one RecordListItem', () =>
 	expect( wrapper ).toMatchSnapshot();
 });
 
-test('should render Compressed RecordListItem', () => {  });
-test('should render Expanded RecordListItem', () => {  });
+test('should render one expanded RecordListItem payment', () => 
+{
+	const record = records[1];
+	const member = members.find( (member) => record.playerUuid === member.playerUuid );
+	
+	const wrapper = shallow(
+		<RecordListItem
+			key={record.id}
+			name={`${member.firstName} ${member.surname}`}
+			seasonName={ seasons[0].seasonName }
+			{...record} 
+		/>
+	);
+	wrapper.find('.list-item').simulate('click');
+
+	expect( wrapper ).toMatchSnapshot();
+});
+
+test('should render one compressed RecordListItem Debt', () => 
+{
+	const record = records[0];
+	const member = members.find( (member) => record.playerUuid === member.playerUuid );
+	
+	const wrapper = shallow(
+		<RecordListItem 
+			key={record.id}
+			name={`${member.firstName} ${member.surname}`}
+			seasonName={ seasons[0].seasonName }
+			{...record} 
+		/>
+	);
+	expect( wrapper ).toMatchSnapshot();
+});
+
+test('should render one expanded RecordListItem payment', () => 
+{
+	const record = records[0];
+	const member = members.find( (member) => record.playerUuid === member.playerUuid );
+	
+	const wrapper = shallow(
+		<RecordListItem
+			key={record.id}
+			name={`${member.firstName} ${member.surname}`}
+			seasonName={ seasons[0].seasonName }
+			{...record} 
+		/>
+	);
+	wrapper.find('.list-item').simulate('click');
+
+	expect( wrapper ).toMatchSnapshot();
+});
