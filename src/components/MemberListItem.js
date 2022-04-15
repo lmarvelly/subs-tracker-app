f0ad56e90ffaa1085cc397cb671acff7ab487f70
Luke Marvelly <lmarvelly@hotmail.com>
2022-04-15 11:56:09 +0100

    Refactoring and adjusting header

--- src/components/MemberListItem.js
+++ src/components/MemberListItem.js
@@ -1,30 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 
-const MemberItem = ( props) =>
+class MemberListItem extends Component
 {
-	
-	return (
-		<div className='list-item'>
-			<div className='list-item__row'>
+	constructor( props )
+	{
+		super( props );
+
+		this.state =
+		{
+			expand: false
+		}
+	}
+
+	render()
+	{
+		const nickname = this.props.nickname ? `'${this.props.nickname}'` : '';
+		const header = (
+			<h3 className='list-item__title'>
+				{`${this.props.firstName} ${nickname} ${this.props.surname}`}
+			</h3>);
+
+		const expandedComponent = (
+			<div>
 				<div className='list-item__column'>
 					<span>
-						Full Name: <span className='bold-font'>{`${props.firstName} ${props.middleNames} ${props.surname}`}</span>
+						Full Name: <span className='bold-font'>{`${this.props.firstName} ${this.props.middleNames} ${this.props.surname}`}</span>
 					</span>
 					<span>
-						Nick Name: <span className='bold-font'>{props.nickname}</span>
+						Nick Name: <span className='bold-font'>{this.props.nickname}</span>
 					</span>
 				</div>
 
 				<Link
 					className='button list-item__align-left' 
-					to={`/edit-member/${props.playerUuid}`}
+					to={`/edit-member/${this.props.playerUuid}`}
 				>
 					Edit Member
 				</Link>
 			</div>
-		</div>
-	);
+		);
+
+		// const mobileComponent = ();
+
+		return (
+			<div className='list-item'>
+				<div className='list-item__row'>
+					{
+						header
+					}
+
+					{
+						this.state.expand && expandedComponent
+					}
+				</div>
+			</div>
+		);
+	}
 }
 
-export default MemberItem;
+export default MemberListItem;
