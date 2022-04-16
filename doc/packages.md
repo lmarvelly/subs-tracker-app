f6b13371485216fd2a5a01e02b108f365594e27c
Luke Marvelly <lmarvelly@hotmail.com>
2022-04-16 13:49:35 +0100

    Updating

--- doc/packages.md
+++ doc/packages.md
@@ -3,6 +3,9 @@ This is a redux middleware so we can run ....
 
 To install this we need to add applyMiddleware from redux in the store. 
 
+### Babel Polyfill
+Needed for older, and useless, browsers like Internet Explorer 
+
 
 # This file is the firebase developement setup
 .env.development 
--- src/tests/components/LoadingPage.test.js
+++ src/tests/components/LoadingPage.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import LoadingPage from "../../components/LoadingPage";
 
-test('should first', () => 
+test('Should render the Loading Page', () => 
 { 
 	const wrapper = shallow(<LoadingPage />);
 	expect(wrapper).toMatchSnapshot();
