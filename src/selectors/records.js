30eb8e6674e566ced69e3d1a589e777c197bf27c
Luke Marvelly <lmarvelly@hotmail.com>
2022-04-18 13:22:31 +0100

    Created improved algorithm for searching throught text

--- src/selectors/records.js
+++ src/selectors/records.js
@@ -16,14 +16,50 @@ import moment from "moment";
  */
 export default ( records = {}, members = {}, { descriptionTextFilter, memberTextFilter, sortBy = 'dateAscending', startDate, endDate, seasonFilter } ) =>
 {
+	// memberName.toLowerCase().includes(memberTextFilter.toLowerCase());
+	
+
 	return records.filter( (record) =>
 	{
 		const member = members.find( ( member ) => 
 			record.playerUuid === member.playerUuid
 		);
-		const memberName = `${member.firstName} ${member.middleNames} ${member.surname} ${member.nickname}`;
-		// TODO: Improve searching through names
-		const idMatch = memberName.toLowerCase().includes(memberTextFilter.toLowerCase());
+
+		// MemberSearch
+		const search = ( filterTextArray = [], searchTextArray = [] ) =>
+		{
+			let result = 0;
+			filterTextArray.forEach(filterText => 
+			{
+				searchTextArray.forEach(searchText => 
+				{
+					if(searchText.toLowerCase().includes(filterText.toLowerCase()))
+					{
+						result += 1;
+					}
+				});
+			});
+
+			return result >= filterTextArray.length;
+		}
+
+		const isNotBlank = ( text ) => 
+		{
+			return (text !== '')
+		}
+
+		const memberTextFilterArray = memberTextFilter.split(' ').filter(isNotBlank);
+
+		const middleNames = member.middleNames.split(' ');
+		const nickname = member.nickname.split(' ');
+		let searchTextArray = [ member.firstName, member.surname ];
+		searchTextArray = searchTextArray.concat(middleNames, nickname);
+		
+		// Filter out blank text of search text and member text
+		searchTextArray = searchTextArray.filter(isNotBlank);
+	
+		const isMatch = search( memberTextFilterArray, searchTextArray );
+
 		const createdAtMoment = moment( record.createdAt );
 		const startDateMatch = startDate ? startDate.isSameOrBefore( createdAtMoment, 'day' ) : true; // if the record is created the same day or before the startDate it gets filtered out
 		const endDateMatch  = endDate ? endDate.isSameOrAfter( createdAtMoment, 'day' ) : true ; // if the record is created the same day or after the endDate then it's filtered out
@@ -31,7 +67,7 @@ export default ( records = {}, members = {}, { descriptionTextFilter, memberText
 		
 		const seasonMatch = seasonFilter ? seasonFilter === record.seasonUuid : true;
 		
-		return idMatch && startDateMatch && endDateMatch && textMatch && seasonMatch; // Return true only if all the above are true. Record is removed if false
+		return isMatch && startDateMatch && endDateMatch && textMatch && seasonMatch; // Return true only if all the above are true. Record is removed if false
 	}).sort( (a, b) => 
 	{
 		if( sortBy === 'dateAscending' ) 
