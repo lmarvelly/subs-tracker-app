4fa0bb98d1c766618e73a774a34ee7ed34c56071
Luke Marvelly <lmarvelly@hotmail.com>
2022-04-19 17:39:27 +0100

    Added test

--- src/tests/reducers/memberFilters.test.js
+++ src/tests/reducers/memberFilters.test.js
@@ -31,3 +31,16 @@ test('should setup Member Filter Reducer to filter members by text', () =>
 
 	expect( state.text ).toBe( text );
 });
+
+test('should reset filters', () => 
+{
+	const text = 'testing';
+	const action = { type: 'SET_MEMBER_TEXT_FILTER', text };
+	let state = filtersReducer( undefined, action );
+	state = filtersReducer( state, { type: 'SORT_ALPHABETIC_ASC' } );
+	
+	// Reset filters
+	state = filtersReducer( state, { type: 'RESET_MEMBER_FILTERS' } );
+
+	expect(state).toEqual({ text: '', sortBy: 'alphabetAsc' });
+});
