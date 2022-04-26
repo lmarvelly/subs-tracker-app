c06472721218b950b944b1c066ef99983d5901dd
Luke Marvelly <lmarvelly@hotmail.com>
2022-04-26 18:58:29 +0100

    Updating and adding code to update Error message when form is complete

--- src/components/RecordForm.js
+++ src/components/RecordForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import moment from 'moment';
 import { SingleDatePicker } from 'react-dates';
 
@@ -27,26 +27,48 @@ export default class RecordForm extends Component
 		};
 	}
 
+	// onBlur = () =>
+	// {
+	// 	this.isFormFalsy();
+	// }
+
+	// Clears error, if everything is fine, and returns true or false
+	isFormFalsy = () =>
+	{
+		const amountPaid = parseFloat(this.state.amountPaid, 10);
+		const amountOwed = parseFloat(this.state.amountOwed, 10);
+		console.log('amountPaid', amountPaid);
+		console.log('amountOwed', amountOwed);
+		console.log('amountOwed > amountPaid', amountOwed > amountPaid);
+		const isDebtAmountsRight = amountOwed > amountPaid;
+		const isAmountOrAmountOwed = (this.state.amount || this.state.amountOwed);
+		const isFalsy = !isDebtAmountsRight || !this.state.description || !isAmountOrAmountOwed || !this.state.playerUuid || !this.state.seasonUuid;
+
+		return isFalsy;
+	}
 	onSeasonNameChange = ( e ) => {
 		const seasonUuid = e.target.value;
 
 		this.setState( () => ({ seasonUuid }) );
+		// this.isFormFalsy();
 	}
 	onNameChange = ( e ) => {
 		const playerUuid = e.target.value;
 		this.setState( () => ({ playerUuid }) );
+		// this.isFormFalsy();
 	};
 	onTypeChange = ( e ) => {
 		const recordType = e.target.value;
 		this.setState( () => ({ recordType }) );
 		if ( recordType === 'PAYMENT') 
 		{
-			this.setState( () => ({ amount: this.state.amountOwed}) );
+			this.setState( { amount: this.state.amountOwed} );
 		}
 		if ( recordType === 'DEBT' ) 
 		{
-			this.setState( () => ({ amountOwed: this.state.amount}) );
+			this.setState( { amountOwed: this.state.amount} );
 		}
+		// this.isFormFalsy();
 	};
 	onDescriptionChange = ( e ) => {
 		const description = e.target.value;
@@ -54,6 +76,7 @@ export default class RecordForm extends Component
 		{
 			this.setState( () => ({ description }) );
 		}
+		// this.isFormFalsy();
 	};
 	onNoteChange = ( e ) => {
 		const note = e.target.value;
@@ -62,7 +85,6 @@ export default class RecordForm extends Component
 			this.setState( () => ({ note }) );
 		}
 	};
-
 	setAmount = ( amount, id ) => {
 		if( !amount || ((amount != 0 || id === 'amountPayed') && amount <= 1000000))
 		{
@@ -70,15 +92,18 @@ export default class RecordForm extends Component
 			switch (id) 
 			{
 				case 'amountToPay':
-					this.setState( () => ({ amount }) );
+					this.setState( { amount } );
+					// this.isFormFalsy();
 					break;
 				
 				case 'amountInDebt':
-					this.setState( () => ({ amountOwed: amount }) );
+					this.setState( { amountOwed: amount } );
+					// this.isFormFalsy();
 					break;
 				
 				case 'amountPayed':
-					this.setState( () => ({ amountPaid: amount }) );
+					this.setState( { amountPaid: amount } );
+					// this.isFormFalsy();
 					break;
 				
 				default:
@@ -86,7 +111,6 @@ export default class RecordForm extends Component
 			}
 		}
 	}
-
 	onAmountChange = ( e ) => 
 	{
 		const id = e.target.id;
@@ -95,6 +119,7 @@ export default class RecordForm extends Component
 		// The amount is not able to be deleted if we do not include this OR statement. We also have the regular expression to prevent the wrong input being entered
 		if( !amount || amount.match(/^\d{1,}(\.\d{0,2})?$/) )
 		{
+			// Don't want message showing for 'Amount paid'
 			if ( amount == 0 && id !== 'amountPayed' ) 
 			{
 				this.setState(() => ({amountError: 'Amount cannot be zero'}));
@@ -105,16 +130,18 @@ export default class RecordForm extends Component
 			}
 			this.setAmount(amount, id);
 		}
+		// this.isFormFalsy();
 	};
 	onDateChange = ( createdAt ) => {
 		if(createdAt) // prevents the user from deleting the date.
 		{
-			this.setState( () => ({ createdAt }) )
+			this.setState( () => ({ createdAt }) );
 		}
 	};
 	onFocusChange = ( { focused } ) => {
 		this.setState( () => ({ calenderFocused: focused }) );
 	};
+
 	onSubmit = ( e ) => {
 		e.preventDefault();
 
@@ -150,13 +177,9 @@ export default class RecordForm extends Component
 			}
 		}
 
-		if ( !this.state.description || !(this.state.amount || this.state.amountOwed) || !this.state.playerUuid || !this.state.seasonUuid )
+		if ( this.isFormFalsy() )
 		{
 			this.setState( () => ({ error: 'Please check details' }) );
-		}
-		else if ( this.state.amountPaid > this.state.amountOwed) 
-		{
-			this.setState( () => ({ error: 'Amount Paid cannot be more than Amount Owed' }) );
 		}
 		else
 		{
@@ -166,7 +189,11 @@ export default class RecordForm extends Component
 		}
 	};
 
-	render(){
+	render()
+	{
+		const amountPaid = parseFloat(this.state.amountPaid, 10);
+		const amountOwed = parseFloat(this.state.amountOwed, 10);
+		const errorDebtClass = amountPaid > amountOwed ? '--error' : '';
 		const moneyInput = (amountErrorClassName) =>
 		{
 			if (this.state.recordType === 'PAYMENT') 
@@ -180,6 +207,7 @@ export default class RecordForm extends Component
 							placeholder="How much was Paid?"
 							value={ this.state.amount }
 							onChange={ this.onAmountChange }
+							// onBlur={this.onBlur}
 						/>
 					</div>
 				)
@@ -188,6 +216,7 @@ export default class RecordForm extends Component
 			{
 				return(
 					<div>
+						{ errorDebtClass && <p className='form__error'>Amount cannot be less than Amount Owed</p>}
 						<input
 							id='amountInDebt'
 							className={`text-input${amountErrorClassName}`}
@@ -195,16 +224,19 @@ export default class RecordForm extends Component
 							placeholder="Total Debt Amount"
 							value={ this.state.amountOwed }
 							onChange={ this.onAmountChange }
+							// onBlur={this.onBlur}
 						/>
+						{ errorDebtClass && <p className='form__error'>Amount Paid cannot be more than Amount Owed</p>}
 						{
 							this.props.record && (
 								<input
 									id='amountPayed'
-									className='text-input'
+									className={`text-input${errorDebtClass}`}
 									type="text"
 									placeholder="Amount of Debt Paid"
 									value={ this.state.amountPaid }
 									onChange={ this.onAmountChange }
+									// onBlur={this.onBlur}
 								/>
 							)
 						}
@@ -218,6 +250,7 @@ export default class RecordForm extends Component
 		const descriptionErrorClassName = this.state.description ? '' : error;
 		const amountErrorClassName = this.state.amount || this.state.amountOwed ? '' : error;
 		// div around Submit button stops it from being directly styled by the form
+		const isFalsy = this.isFormFalsy();
 		return(
 			<div>
 				<form className='form' onSubmit={ this.onSubmit }>
@@ -227,6 +260,7 @@ export default class RecordForm extends Component
 						className={`select${seasonErrorClassName}`}
 						onChange={ this.onSeasonNameChange }
 						value={ this.state.seasonUuid }
+						// onBlur={this.onBlur}
 					>
 						<option hidden>Select a Season</option>
 						{
@@ -249,6 +283,7 @@ export default class RecordForm extends Component
 						className={`select${memberErrorClassName}`}
 						onChange={ this.onNameChange }
 						value={ this.state.playerUuid }
+						// onBlur={this.onBlur}
 					>
 						<option hidden >Select a Member</option>
 						{
@@ -270,6 +305,7 @@ export default class RecordForm extends Component
 						className='select'
 						onChange={ this.onTypeChange }
 						value={ this.state.recordType }
+						// onBlur={this.onBlur}
 					>
 						<option 
 							value="PAYMENT"
@@ -290,6 +326,7 @@ export default class RecordForm extends Component
 						placeholder="Provide a Description"
 						value={ this.state.description }
 						onChange={ this.onDescriptionChange }
+						// onBlur={this.onBlur}
 					/>
 					
 					{this.state.amountError && <p className='form__error'>{this.state.amountError}</p>}
@@ -313,7 +350,7 @@ export default class RecordForm extends Component
 						value={this.state.note}
 					>
 					</textarea>
-					{this.state.error && <p className='form__error'>{this.state.error}</p>}
+					{(this.state.error && isFalsy ) && <p className='form__error'>{this.state.error}</p>}
 					<div>
 						<button className='button'>
 							{this.props.record ? 'Save Changes' : 'Add Payment Record'}
