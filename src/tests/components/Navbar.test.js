ba1314ba5ea7548bb64670978e47872c093c3f64
Luke Marvelly <lmarvelly@hotmail.com>
2022-05-03 12:10:42 +0100

    Added Tests for rendering Navbar component

--- src/tests/components/Navbar.test.js
+++ src/tests/components/Navbar.test.js
@@ -2,8 +2,54 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Navbar from '../../components/Navbar';
 
-test('should render Navbar correctly', () =>
+let wrapper;
+
+beforeEach( () =>
 {
-	const wrapper = shallow(<Navbar />)
+	wrapper = shallow(<Navbar />);
+});
+
+test('should render Navbar button correctly', () =>
+{
+	expect(wrapper).toMatchSnapshot();
+});
+
+test('should render Navbar when Navbar button is clicked', () =>
+{
+	// Not working for some reason
+	// const navButton = wrapper.find('#navButton').at(0);
+	// navButton.simulate('click');
+
+	wrapper.setState({navHidden: false});
+
+	expect(wrapper).toMatchSnapshot();
+});
+
+test('should render Records dropdown links', () => 
+{
+	wrapper.setState({navHidden: false});
+	// Buttons cannot be found for some reason
+	// const recordButton = wrapper.find('.recordButton').at(0);
+	// recordButton.simulate('click');
+	
+	// Setting state instead
+	wrapper.setState({recordNavHidden: false});
+
+	expect(wrapper).toMatchSnapshot();
+});
+test('should render Members dropdown links', () => 
+{
+	wrapper.setState({navHidden: false});
+	
+	wrapper.setState({memberNavHidden: false});
+
+	expect(wrapper).toMatchSnapshot();
+});
+test('should render Seasons dropdown links', () => 
+{
+	wrapper.setState({navHidden: false});
+	
+	wrapper.setState({seasonNavHidden: false});
+
 	expect(wrapper).toMatchSnapshot();
 });
