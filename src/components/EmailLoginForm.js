27b86777c6519ff6d92c0ed8766d4a1fd62b32ba
Luke Marvelly <lmarvelly@hotmail.com>
2022-05-11 16:17:52 +0100

    Styling buttons

--- src/components/EmailLoginForm.js
+++ src/components/EmailLoginForm.js
@@ -4,22 +4,52 @@ export default class EmailLoginForm extends Component
 {
 	constructor( props )
 	{
-		super( props )
+		super( props );
+
+		this.state =
+		{
+			email: undefined,
+			password: undefined,
+
+			displayEmailLogin: false,
+			displayEmailSignUp: false
+		}
 	}
 
 	render()
 	{
-		return (
-			<form>
+		const inputs = (
+			<div>
 				<input 
+					className={`text-input`}
 					placeholder='email@example.com'
 					type="text" 
 				/>
 				<input
+					className={`text-input`}
 					placeholder='Password'
 					type="password"
-					
 				/>
+			</div>
+		);
+
+		return (
+			<form className='form'>
+				<button
+					className='button'
+				>
+					Login with email
+				</button>
+				<button
+					className='button--email-signup'
+				>
+					Sign up with email
+				</button>
+				<button
+					className='button--back'
+				>
+					back
+				</button>
 			</form>
 		);
 	}
