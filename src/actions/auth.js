3125618c1521aa04590bc494f59dd066e2585089
Luke Marvelly <lmarvelly@hotmail.com>
2022-05-17 15:41:24 +0100

    Added Error Handling

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -29,7 +29,27 @@ export const startCreateUserWithEmail = ( email, password ) =>
 		auth.signOut();
 		alert(`Email sent to ${email}`);
 	})
-	.catch('error');
+	.catch(error =>
+	{
+		switch (error.code)
+		{	
+			case 'auth/email-already-in-use':
+				console.log(`Email address ${email} already in use.`);
+				break;
+			case 'auth/invalid-email':
+				console.log(`Email address ${email} is invalid.`);
+				break;
+			case 'auth/operation-not-allowed':
+				console.log(`Error during sign up.`);
+				break;
+			case 'auth/weak-password':
+				console.log('Password is not strong enough. Add additional characters including special characters and numbers.');
+				break;
+			default:
+				console.log(error.message);
+				break;
+		}
+	});
 }
 
 export const logout = () => (
