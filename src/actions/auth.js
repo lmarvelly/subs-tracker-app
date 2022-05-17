9937c97a34541b9c541eebee130d681b7e939b46
Luke Marvelly <lmarvelly@hotmail.com>
2022-05-17 16:01:42 +0100

    Refactoring error logs into alerts for user

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -34,16 +34,16 @@ export const startCreateUserWithEmail = ( email, password ) =>
 		switch (error.code)
 		{	
 			case 'auth/email-already-in-use':
-				console.log(`Email address ${email} already in use.`);
+				alert(`Email address ${email} already in use.`);
 				break;
 			case 'auth/invalid-email':
-				console.log(`Email address ${email} is invalid.`);
+				alert(`Email address ${email} is invalid.`);
 				break;
 			case 'auth/operation-not-allowed':
-				console.log(`Error during sign up.`);
+				alert(`Apologies, an error occured during sign up.`);
 				break;
 			case 'auth/weak-password':
-				console.log('Password is not strong enough. Add additional characters including special characters and numbers.');
+				alert('Password is not strong enough. Add additional characters including special characters and numbers.');
 				break;
 			default:
 				console.log(error.message);
