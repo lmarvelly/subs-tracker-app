fa00aaade6a6ed4157150253e33d35fe772e7e73
Luke Marvelly <lmarvelly@hotmail.com>
2022-05-24 16:18:08 +0100

    Adding front-end email validation

--- src/components/EmailLoginForm.js
+++ src/components/EmailLoginForm.js
@@ -11,7 +11,7 @@ export default class EmailLoginForm extends Component
 			email: '',
 			password: '',
 			displayType: 'CHOICE_BUTTONS',
-			error: props.error ? props.error : ''
+			formError: props.error ? props.error : ''
 		}
 	}
 
@@ -26,7 +26,7 @@ export default class EmailLoginForm extends Component
 		{
 			displayType: 'CHOICE_BUTTONS',
 			email: '',
-			error: '',
+			formError: '',
 			password: ''
 		});
 		this.props.clearError();
@@ -52,6 +52,20 @@ export default class EmailLoginForm extends Component
 		this.setState({ password: e.target.value });
 	}
 
+	isEmailValid = () =>
+	{
+		const emailPattern = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+		
+		if(this.state.email.match(emailPattern))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
 	// TODO: Form Validation
 	onSubmit = ( e ) =>
 	{
@@ -59,14 +73,30 @@ export default class EmailLoginForm extends Component
 		const email = this.state.email;
 		const password = this.state.password;
 
-		if ( this.state.displayType === 'EMAIL_LOGIN' ) 
+		console.log('not email', !email);
+		console.log('not password', !password);
+		console.log('not is email valid', !this.isEmailValid());
+
+		if ( !email || !password ) 
 		{
-			this.props.emailLogin( email, password );
+			this.setState(() => ({ formError: 'Please check details' }));
 		}
-		if (this.state.displayType === 'EMAIL_SIGN_UP' ) 
+		else if ( !this.isEmailValid() ) 
 		{
-			this.props.createUserWithEmail( email, password );
-			this.setState({displayType: 'EMAIL_LOGIN'});
+			this.setState(() => ({ formError: 'Please check email' }));
+		}
+		else
+		{
+			console.log('Logging in...');
+			if ( this.state.displayType === 'EMAIL_LOGIN' ) 
+			{
+				this.props.emailLogin( email, password );
+			}
+			if (this.state.displayType === 'EMAIL_SIGN_UP' ) 
+			{
+				this.props.createUserWithEmail( email, password );
+				this.setState({displayType: 'EMAIL_LOGIN'});
+			}
 		}
 	}
 
@@ -96,37 +126,39 @@ export default class EmailLoginForm extends Component
 		);
 
 		const error = '__error';
-		const emailErrorName = this.state.email ? '' : error;
+		const emailErrorName = this.isEmailValid() ? '' : error;
 		const passwordErrorName = this.state.password ? '' : error;
+
+		const errorMessage = this.props.error ? this.props.error : this.state.formError;
 
 		const loginSignUpForm = (
 			<div>
-			<form className='form' onSubmit={ this.onSubmit }>
-				{ this.props.error && <p className='form__error'>{this.props.error}</p> }
-				{ this.state.error && emailErrorName && <p className='form__error'>Please enter an email</p> }
-				<input 
-					className={`text-input`}
-					placeholder='email@example.com'
-					type="text"
-					value={this.state.email}
-					onChange={this.onEmailChange}
-				/>
-				{ this.state.error && passwordErrorName && <p className='form__error'>Please enter a password</p> }
-				<input
-					className={`text-input`}
-					placeholder='Password'
-					type="password"
-					value={this.state.password}
-					onChange={this.onPasswordChange}
-				/>
-				<button className='button'>{ (this.state.displayType === 'EMAIL_SIGN_UP') ? 'Sign Up' : 'Login'}</button>
-			</form>
-			<button
-				className='button--back'
-				onClick={this.handleBackToEmailChoiceButtons}
-			>
-				back
-			</button>
+				<form className='form' onSubmit={ this.onSubmit }>
+					{ emailErrorName && <p className='form__error'>Please enter your email</p> }
+					<input 
+						className={`text-input`}
+						placeholder='email@example.com'
+						type="text"
+						value={this.state.email}
+						onChange={this.onEmailChange}
+					/>
+					{ this.state.formError && passwordErrorName && <p className='form__error'>Please enter a password</p> }
+					<input
+						className={`text-input`}
+						placeholder='Password'
+						type="password"
+						value={this.state.password}
+						onChange={this.onPasswordChange}
+					/>
+					{ errorMessage && <p className='form__error'>{errorMessage}</p> }
+					<button className='button'>{ (this.state.displayType === 'EMAIL_SIGN_UP') ? 'Sign Up' : 'Login'}</button>
+				</form>
+				<button
+					className='button--back'
+					onClick={this.handleBackToEmailChoiceButtons}
+				>
+					back
+				</button>
 			</div>
 		);
 
