376f5102eaac14368e322c86bca1f745bfe54836
Luke Marvelly <lmarvelly@hotmail.com>
2022-05-24 18:35:15 +0100

    Adding tests

--- src/tests/components/EmailLoginForm.test.js
+++ src/tests/components/EmailLoginForm.test.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { EmailLoginForm } from '../../components/EmailLoginForm';
+import EmailLoginForm from '../../components/EmailLoginForm';
 
-let wrapper;
+let optionsWrapper, signInWrapper;
 
 beforeEach(() =>
 {
-	wrapper = shallow(<EmailLoginForm />);
+	optionsWrapper = shallow(<EmailLoginForm />);
+
+	signInWrapper = shallow(<EmailLoginForm />);
+	signInWrapper.setState({ displayType: 'EMAIL_SIGN_UP' });
 });
 
-test('should render Email Login Component correctly', () => 
+test('should render Email Login Options correctly', () => 
 {
-	expect(wrapper).toMatchSnapshot();
+	expect(optionsWrapper).toMatchSnapshot();
 });
+
+test('should render Email Sign Up form', () => 
+{
+	expect(signInWrapper).toMatchSnapshot();
+});
+
+// Doesn't work. Might be because of match validation
+test('should show error message for invalid email', () =>
+{
+	const input = signInWrapper.find('input').at(0);
+	const text = 'example';
+	input.simulate('change', 
+	{ 
+		target: { value: text }
+	});
+	// signInWrapper.setState({email: text});
+
+	expect(signInWrapper.state('formError')).toBe('Please Enter a vaild email');
+});
+
+// write test for missing email
+
+// Write test for missing passowrd
