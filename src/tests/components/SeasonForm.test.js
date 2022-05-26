8f9680cfb8613786bc1c5ed70f5c31bce7ebbfea
Luke Marvelly <lmarvelly@hotmail.com>
2022-05-26 18:26:33 +0100

    Added tests

--- src/tests/components/SeasonForm.test.js
+++ src/tests/components/SeasonForm.test.js
@@ -3,17 +3,19 @@ import { shallow } from 'enzyme';
 import SeasonForm from '../../components/SeasonForm';
 import { seasons } from '../fixtures/fixures';
 
-let blankWrapper, completedWrapper, onSubmit;
+let blankWrapper, completedWrapper, onSeasonNameChange, onSubmit;
 
 beforeEach( () =>
 {
 	// jest onChange functions
 	onSubmit = jest.fn();
+	onSeasonNameChange = jest.fn();
 
 	blankWrapper = shallow( <SeasonForm /> );
 	completedWrapper = shallow(
 		<SeasonForm 
 			season={seasons[0]}
+			onSeasonNameChange={onSeasonNameChange}
 			onSubmit={onSubmit}
 		/>
 	);
@@ -40,3 +42,26 @@ test('shouldn\'t show error on submition of completed Member form ', () =>
 });
 
 // Test incomplete form
+test('should render error for not inputing a season name on form submission', () => 
+{
+	blankWrapper.find('form').simulate('submit', 
+	{
+		preventDefault: () => {}
+	});
+	expect( blankWrapper.state('error').length ).toBeGreaterThan(0);
+	expect(blankWrapper).toMatchSnapshot();
+});
+
+test('should change Season Name', () => 
+{
+	const name = 'Season 1';
+	const input = blankWrapper.find('input');
+	input.simulate('change', 
+	{
+		target: { value: name }
+	});
+
+	expect( blankWrapper ).toMatchSnapshot();
+
+	expect( blankWrapper.state('seasonName') ).toEqual(name);
+});
