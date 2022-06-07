7501577226ccae82c2d0befc504512baff2e7e4e
Luke Marvelly <lmarvelly@hotmail.com>
2022-06-07 12:29:16 +0100

    Refactoring and adding Email Login Tests

--- src/tests/components/EmailLoginForm.test.js
+++ src/tests/components/EmailLoginForm.test.js
@@ -3,33 +3,52 @@ import { shallow } from 'enzyme';
 import EmailLoginForm from '../../components/EmailLoginForm';
 
 let 
-	createUserWithEmail, 
+	createUserWithEmail,
+	emailLogin,
 	optionsWrapper, 
-	emailSignInWrapper,
+	emailLoginWrapper,
 	emailSignUpWrapper;
 
 beforeEach(() =>
 {
 	createUserWithEmail = jest.fn();
+	emailLogin = jest.fn();
 
-	emailSignInWrapper = shallow(<EmailLoginForm />);
-	emailSignInWrapper.setState({ displayType: 'EMAIL_LOGIN' });
-
-	optionsWrapper = shallow(<EmailLoginForm />);
+	emailLoginWrapper = shallow(
+		<EmailLoginForm emailLogin={emailLogin} />);
+	emailLoginWrapper.setState({ displayType: 'EMAIL_LOGIN' });
 
 	emailSignUpWrapper = shallow(
 		<EmailLoginForm createUserWithEmail={createUserWithEmail} />);
 	emailSignUpWrapper.setState({ displayType: 'EMAIL_SIGN_UP' });
+
+	optionsWrapper = shallow(<EmailLoginForm />);
 });
+
+/////////////////////////////////////
+// EMAIL LOGIN OPTIONS RENDER TEST //
+/////////////////////////////////////
 
 test('should render Email Login Options correctly', () => 
 {
 	expect(optionsWrapper).toMatchSnapshot();
 });
 
+////////////////////////////////////
+// EMAIL SIGN UP/LOGIN FORM TESTS //
+////////////////////////////////////
+
+// No need to do tests for both Sign Up and Login input errors as
+// the form is the same. Only the function called when form is 
+// submitted is different.
 test('should render Email Sign Up form', () => 
 {
 	expect(emailSignUpWrapper).toMatchSnapshot();
+});
+
+test('should render Email Login Form', () =>
+{
+	expect(emailLoginWrapper).toMatchSnapshot();
 });
 
 // Should have 'Please check details' above Sign Up button
@@ -88,7 +107,12 @@ test('should show error message for missing details', () =>
 	expect(emailSignUpWrapper.state('formError')).toBe('Please check details');
 });
 
-// Check if this.props.createUserWithEmail gets called
+
+///////////////////////////////
+// EMAIL SIGN UP SUBMIT TEST //
+///////////////////////////////
+
+// Check if this.props.createUserWithEmail gets called on Submit
 test('should call createUserWithEmail() from props', () =>
 {
 	const email = emailSignUpWrapper.find('input').at(0);
@@ -107,11 +131,35 @@ test('should call createUserWithEmail() from props', () =>
 	{
 		preventDefault: () => {}
 	});
-	// expect(emailSignUpWrapper).toMatchSnapshot();
+
 	expect(createUserWithEmail).toHaveBeenCalled();
 });
 
-// Test for rendering Email Sign Up
 
+//////////////////////////////
+// EMAIL LOGIN SUBMIT TESTS //
+//////////////////////////////
 
-// Tests for Submitting Email Sign Up details
+// Tests for Submitting Email Login details
+test('should call emailLogin() when submitted', () => 
+{
+	const email = emailLoginWrapper.find('input').at(0);
+	email.simulate('change', 
+	{ 
+		target: { value: 'example@email.com' }
+	});
+
+	const password = emailLoginWrapper.find('input').at(1);
+	password.simulate('change',
+	{
+		target: { value: 'password' }
+	});
+
+	emailLoginWrapper.find('form').simulate('submit', 
+	{
+		preventDefault: () => {}
+	});
+
+	expect(emailLogin).toHaveBeenCalled();
+});
+
