3f9e2223b347d9462901540bd25165442ea0d309
Luke Marvelly <lmarvelly@hotmail.com>
2022-06-26 18:22:11 +0100

    Removing bad way to implement session

--- src/tests/reducers/session.test.js
+++
@@ -1,9 +0,0 @@
-import sessionReducer from '../../reducers/session';
-import { session1 } from '../fixtures/fixures';
-
-test('should set default state', () =>
-{
-	const state = sessionReducer( undefined, { type: '@@INIT' } );
-
-	expect( state ).toEqual([]);
-});
