f63ba955d2634f588730643988a531bb9d65824e
Luke Marvelly <lmarvelly@hotmail.com>
2022-06-28 16:44:39 +0100

    Adding new tests

--- src/tests/components/SessionForm.test.js
+++ src/tests/components/SessionForm.test.js
@@ -2,18 +2,37 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import moment from 'moment';
 import SessionForm from '../../components/SessionForm';
-import { members, seasons } from '../fixtures/fixures';
+import { members, seasons, sessionArray } from '../fixtures/fixures';
 
-let emptyWrapper, wrapper;
+let emptyWrapper,
+	onAmountChange,
+	sessionArrayWrapper,
+	wrapper;
 
 beforeEach( () =>
 {
+	onAmountChange = jest.fn();
+
 	emptyWrapper = shallow(
 		<SessionForm members={[]} seasons={[]} />
 	);
 
 	wrapper = shallow(
-		<SessionForm members={members} seasons={seasons} />
+		<SessionForm
+			members={members}
+			seasons={seasons}
+			onAmountChange={onAmountChange}
+		/>
+	);
+
+	sessionArrayWrapper = shallow(
+		<SessionForm
+			members={members}
+			seasons={seasons}
+			sessionArray={sessionArray}
+			onAmountChange={onAmountChange}
+			
+		/>
 	);
 });
 
@@ -37,7 +56,7 @@ test('should render error for not inputing any values form submission', () =>
 	expect( wrapper ).toMatchSnapshot();
 });
 
-test('should only render season and amount error messages', () => 
+test('should only render season, amount and add member error messages', () => 
 {
 	// description input
 	wrapper.find('#description').simulate('change',
@@ -50,5 +69,141 @@ test('should only render season and amount error messages', () =>
 		preventDefault: () => {}
 	});
 	
+	expect( wrapper.state('error').length ).toBeGreaterThan(0);
 	expect(wrapper).toMatchSnapshot();
 });
+
+test('should only render error message for description and add member', () => 
+{
+	// Season
+	const uuid = seasons[0].seasonUuid;
+	const input = wrapper.find('#seasonName');
+	input.simulate('change', { target: { value: uuid } });
+	// Amount
+	const input2 = wrapper.find('#amountToPay');
+	input2.simulate('change', { target: {value: '4'} });
+
+	// submit
+	wrapper.find('form').simulate('submit', 
+	{
+		preventDefault: () => {}
+	});
+
+	expect( wrapper.state('error').length ).toBeGreaterThan(0);
+	expect(wrapper).toMatchSnapshot();
+});
+
+test('should render "Please enter an Amount" error message for no amount entered', () => 
+{
+	// Season input
+	const seasonUuid = seasons[0].seasonUuid;
+	const input = wrapper.find('#seasonName');
+	input.simulate('change', { target: { value: seasonUuid } });
+	// description input
+	wrapper.find('#description').simulate('change',
+	{
+		target: { value: 'New description' }
+	});
+
+	// submit
+	wrapper.find('form').simulate('submit', 
+	{
+		preventDefault: () => {}
+	});
+
+	expect(wrapper).toMatchSnapshot();
+});
+
+test('should not render "Please add members" error message', () => 
+{
+	// Season input
+	const seasonUuid = seasons[0].seasonUuid;
+	const input = sessionArrayWrapper.find('#seasonName');
+	input.simulate('change', { target: { value: seasonUuid } });
+	// description input
+	sessionArrayWrapper.find('#description').simulate('change',
+	{
+		target: { value: 'New description' }
+	});
+	// Amount input
+	const input2 = sessionArrayWrapper.find('#amountToPay');
+	input2.simulate('change', { target: {value: '4'} });
+
+	sessionArrayWrapper.setState({sessionArray})
+
+	// submit
+	sessionArrayWrapper.find('form').simulate('submit', 
+	{
+		preventDefault: () => {}
+	});
+
+	// console.log(sessionArrayWrapper.state('sessionArray'));
+
+	expect(sessionArrayWrapper).toMatchSnapshot();
+});
+
+
+
+test('should set description on input change', () => 
+{
+	const value = 'New description';
+	wrapper.find('input').at(0).simulate('change',
+	{
+		target: { value } // Setting the value of e.target.value
+	});
+
+	expect(wrapper.state('description')).toBe(value);
+});
+
+test('should set note on textarea change', () =>
+{
+	const value = 'New Note';
+	wrapper.find('textarea').at(0).simulate('change',
+	{
+		target: { value } // setting the value of e.target.value
+	});
+
+	expect(wrapper.state('note')).toBe(value);
+});
+
+test('Should set amount if input data is valid', () =>
+{
+	const value = '1';
+	const input = wrapper.find('#amountToPay');
+	
+	input.simulate('change',
+	{
+		target: { value }
+	});
+
+	expect(wrapper.state('amount')).toEqual(value);
+});
+
+test('shouldnt let user pay more than 10,000', () => 
+{
+	const value = '10001';
+	const input = wrapper.find('#amountToPay');
+	input.simulate('change',
+	{
+		target: { value }
+	});
+
+	expect(input.value).toEqual(undefined);
+	expect(wrapper.state('amount')).toEqual('');
+});
+
+test('Should not set amount because input has too many decimal places', () =>
+{
+	const value = '9.001';
+	const input = wrapper.find('input').at(0);
+
+	input.simulate('change',
+	{
+		target: { value }
+	});
+
+	expect(input.value).toEqual(undefined);
+	expect(wrapper.state('amount')).toBe('');
+});
+
+// Add empty session array to tests 
