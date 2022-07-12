fc3085daf910f1be59af18ee652e1a863a88a8bd
Luke Marvelly <lmarvelly@hotmail.com>
2022-07-12 13:15:11 +0100

    Added test for adding new Session to Database

--- src/tests/actions/otherSettings.test.js
+++ src/tests/actions/otherSettings.test.js
@@ -1,20 +1,68 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 
-import { addSessionType } from '../../actions/otherSettings';
-import { sessionTypeList } from '../fixtures/fixures';
+import { addSessionType, startAddSessionType } from '../../actions/otherSettings';
+import { sessionTypes } from '../fixtures/fixures';
 import database from '../../firebase/firebase';
 
 const uid = 'testuid';
 const defaultAuthState = { auth: { uid }}
 const createMockStore = configureMockStore([thunk]);
 
+beforeEach((done) =>
+{
+
+	const sessionTypeData = {};
+	sessionTypes.forEach(({ sessionUuid, sessionName }) =>
+	{
+		sessionTypeData[sessionUuid] = { sessionName };
+	});
+
+	database.ref(`subs-tracker/users/${uid}/session_types`)
+		.set(sessionTypeData)
+		.then(() => done());
+});
+
 test('should create a Add Session Type Action Objest', () =>
 {
-	const action = addSessionType(sessionTypeList[0]);
+	const action = addSessionType(sessionTypes[0]);
 	expect(action).toEqual(
 	{
 		type: 'ADD_SESSION_TYPE',
-		sessionType: sessionTypeList[0]
+		sessionType: sessionTypes[0]
+	});
+});
+
+test('should add a new Session to the database', () =>
+{
+	const store = createMockStore(defaultAuthState);
+
+	const sessionType = 
+	{
+		sessionName: 'Kickabout'
+	};
+
+	const promise = store.dispatch(startAddSessionType(sessionType)).then(() =>
+	{
+		const actions = store.getActions();
+		expect(actions[0]).toEqual(
+		{
+			type: 'ADD_SESSION_TYPE',
+			sessionType:
+			{
+				sessionUuid: expect.any(String),
+				...sessionType
+			}
+		});
+
+		return database.ref(`subs-tracker/users/${uid}/session_types/${actions[0].sessionType.sessionUuid}`).once('value');
+	});
+
+	promise.then((snapshot) =>
+	{
+		expect(snapshot.val()).toEqual(
+			...sessionType
+		);
+		done();
 	});
 });
