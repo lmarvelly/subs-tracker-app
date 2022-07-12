5c519ac8a971efffb8b2b13333e3344ae23b5423
Luke Marvelly <lmarvelly@hotmail.com>
2022-07-12 13:13:15 +0100

    Added function to add new action to database

--- src/actions/otherSettings.js
+++ src/actions/otherSettings.js
@@ -5,3 +5,22 @@ export const addSessionType = ( sessionType ) => (
 	type: 'ADD_SESSION_TYPE',
 	sessionType
 });
+
+export const startAddSessionType = ( sessionType = '' ) =>
+{
+	return ( dispatch, getState ) =>
+	{
+		const uid = getState().auth.uid;
+
+		return database.ref(`subs-tracker/users/${uid}/session_types`)
+			.push(sessionType)
+			.then((ref) => 
+			{
+				dispatch(addSessionType(
+				{
+					sessionUuid: ref.key,
+					...sessionType
+				}));
+			});
+	}
+}
