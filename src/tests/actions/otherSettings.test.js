d5f018c907a40422ade11650866299787f82d310
Luke Marvelly <lmarvelly@hotmail.com>
2022-07-12 09:51:18 +0100

    Updated Test

--- src/tests/actions/otherSettings.test.js
+++ src/tests/actions/otherSettings.test.js
@@ -2,19 +2,19 @@ import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 
 import { addSessionType } from '../../actions/otherSettings';
+import { sessionTypeList } from '../fixtures/fixures';
 import database from '../../firebase/firebase';
 
 const uid = 'testuid';
 const defaultAuthState = { auth: { uid }}
 const createMockStore = configureMockStore([thunk]);
-const sessionList = [ 'Training', 'GFSN Game' ];
 
 test('should create a Add Session Type Action Objest', () =>
 {
-	const action = addSessionType(sessionList[0]);
+	const action = addSessionType(sessionTypeList[0]);
 	expect(action).toEqual(
 	{
 		type: 'ADD_SESSION_TYPE',
-		sessionType: sessionList[0]
+		sessionType: sessionTypeList[0]
 	});
 });
