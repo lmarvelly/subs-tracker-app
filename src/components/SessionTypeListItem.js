8089278114283fe2afde26433260ea8b1cd2bbad
Luke Marvelly <lmarvelly@hotmail.com>
2022-07-13 16:22:04 +0100

    Added handling for Editing Session Type

--- src/components/SessionTypeListItem.js
+++ src/components/SessionTypeListItem.js
@@ -10,13 +10,26 @@ const SessionTypeListItem = ( props ) =>
 		}
 	}
 
+	const handleEdit = () =>
+	{
+		const sessionName = prompt('Edit Session Name', props.sessionName);
+
+		const updates = {sessionName};
+
+		if (sessionName) 
+		{
+			props.editSessionType( props.sessionUuid, updates );
+		}
+	}
+
 	return(
 		<div className='list-item'>
 			<div className='list-item__row'>
 				<h3 className={`list-item__title`}>
 					{props.sessionName}
 				</h3>
-				<button onClick={handleRemove} className='button'>remove</button>
+				<button onClick={handleEdit} className='button'>Edit</button>
+				<button onClick={handleRemove} className='button'>Remove</button>
 			</div>
 		</div>
 	);
