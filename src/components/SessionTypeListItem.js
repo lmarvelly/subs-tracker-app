7a8cc12d6b1ad8ae49e617a63199870e9bdc0a09
Luke Marvelly <lmarvelly@hotmail.com>
2022-07-13 13:57:21 +0100

    Adding button and handling to remove Session Type

--- src/components/SessionTypeListItem.js
+++ src/components/SessionTypeListItem.js
@@ -2,13 +2,21 @@ import React from 'react';
 
 const SessionTypeListItem = ( props ) =>
 {
+	const handleRemove = () =>
+	{
+		if(confirm(`Are you sure you want to remove '${props.sessionName}'`))
+		{
+			props.removeSessionType( props.sessionUuid );
+		}
+	}
+
 	return(
 		<div className='list-item'>
 			<div className='list-item__row'>
 				<h3 className={`list-item__title`}>
 					{props.sessionName}
 				</h3>
-				
+				<button onClick={handleRemove} className='button'>remove</button>
 			</div>
 		</div>
 	);
