ffc4b6a3d8311b4edc741dd78207cc966b5c9add
Luke Marvelly <lmarvelly@hotmail.com>
2022-07-27 17:15:41 +0100

    Changing subtitle

--- src/components/AddSessionPage.js
+++ src/components/AddSessionPage.js
@@ -85,7 +85,7 @@ export class AddSessionPage extends Component
 						<div className='page-header__content-column'>
 							<div>
 								<h1 className='page-header__title'>Add Session Page</h1>
-								<span>Create multiple records for a single session</span>
+								<span>Add a new Group Session</span>
 							</div>
 						</div>
 					</div>
