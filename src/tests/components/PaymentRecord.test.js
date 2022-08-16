36af62d00f7f666873727ccfe66c0ef41480cf09
Luke Marvelly <lmarvelly@hotmail.com>
2022-08-16 09:51:36 +0100

    Updating test

--- src/tests/components/PaymentRecord.test.js
+++ src/tests/components/PaymentRecord.test.js
@@ -3,12 +3,14 @@ import { shallow } from 'enzyme';
 
 // Importing the named export. To dynamically pass in props and make sure it's rendering correctly.
 import { PaymentRecord } from '../../components/PaymentRecord';
-import { records, members, seasons } from '../fixtures/fixures';
+import { records, members, seasons, sessions } from '../fixtures/fixures';
+
+const allRecords = records.concat(sessions);
 
 test('should render PaymentRecord with Payments/Debts', () => 
 {
 	const wrapper = shallow( 
-		<PaymentRecord paymentRecord={records} members={members} seasons={seasons} />
+		<PaymentRecord paymentRecord={records} members={members} seasons={seasons} sessions={sessions} allRecords={allRecords} />
 	);
 
 	expect(wrapper).toMatchSnapshot();
@@ -16,7 +18,7 @@ test('should render PaymentRecord with Payments/Debts', () =>
 
 test('should render PaymentRecord with no Payments/Debts', () =>
 {
-  const wrapper = shallow(<PaymentRecord paymentRecord={[]} />);
+  const wrapper = shallow(<PaymentRecord paymentRecord={[]} sessions={[]} allRecords={[]} />);
 
   expect(wrapper).toMatchSnapshot();
 });
