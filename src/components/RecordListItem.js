d519bca0a3ac25815a64ef22dfbc96fa56dbc537
Luke Marvelly <lmarvelly@hotmail.com>
2022-09-11 16:04:03 +0100

    Removing old Debt features

--- src/components/RecordListItem.js
+++ src/components/RecordListItem.js
@@ -23,65 +23,13 @@ class RecordListItem extends Component
 
 		this.state =
 		{
-			amountPaid: this.props.amountPaid / 100,
+			amount: this.props.amount / 100,
 			error: '',
 			expand: false,
 			stayExpanded: false
 		}
 
 		this.clickListItemHandler = this.clickListItemHandler.bind( this );
-		this.onDebtClick = this.onDebtClick.bind( this );
-	}
-
-	onDebtClick( e )
-	{
-		this.setState(() => ({stayExpanded: true}));
-		
-		const amountOwed = this.props.amountOwed;
-		const amountPaid = this.props.amountPaid;
-		const parsedAmountOwed = parseFloat(amountOwed / 100).toFixed(2);
-		const parsedAmountPaid = parseFloat(amountPaid / 100).toFixed(2);
-
-		const input = prompt(`Debt Amount: £${parsedAmountOwed}. Please make a payment of £${parsedAmountOwed} or less`, parsedAmountPaid);
-
-		if ( input !== null && input !== '' && (!input || input.match(/^\d{1,}(\.\d{0,2})?$/))) 
-		{
-			if ((input * 100) > amountOwed) 
-			{
-				alert(`£${parseFloat(input).toFixed(2)} is more than the debt of £${parsedAmountOwed}`);
-			}
-			else
-			{
-				// Submit new debt payment
-				const parsedInput = parseFloat(input, 10).toFixed(2);
-				const amountPaid = parsedInput * 100;
-				const record = { ...this.props.record, amountPaid };
-				this.props.onSubmit( record );
-
-				if (amountPaid === amountOwed) 
-				{
-					alert(`Player has paid off his debt of £${parsedAmountOwed}`);
-				}
-				else
-				{
-					alert(`Player has paid £${parsedInput} off his debt of £${parsedAmountOwed}`);
-				}
-			}
-		}
-		else if (input === '') 
-		{
-			alert(`Cannot enter an empty string`)
-		}
-		else if (input) 
-		{
-			alert(`${input} is invalid`);
-		}
-
-		// Prevent accidently shrinking component
-		setTimeout(() => 
-		{
-			this.setState(() => ({stayExpanded: false}));
-		}, 250);
 	}
 
 	clickListItemHandler(e)
@@ -140,7 +88,10 @@ class RecordListItem extends Component
 						{
 							this.props.recordType === 'PAYMENT' && (
 								<h3 className={`list-item__data${classCollapsedName}`}>
-									Payment Amount: <span className='bold-font'>{`£${numeral(this.props.amount / 100).format('0,0.00')}` }</span>
+									Payment Amount:
+									<span className='bold-font'>
+										{` £${numeral(this.props.amount / 100).format('0,0.00')}` }
+									</span>
 								</h3>
 							)
 						}
@@ -150,13 +101,7 @@ class RecordListItem extends Component
 									<h3 className={`list-item__data${classCollapsedName}`}>
 										Debt Amount:
 										<span className='bold-font'> 
-											{` £${numeral(this.props.amountOwed / 100).format('0,0.00')}`}
-										</span>
-									</h3>
-									<h3 className='list-item__data'>
-										Debt Paid: 
-										<span className='bold-font'> 
-											{` £${numeral(this.props.amountPaid / 100).format('0,0.00')}`}
+											{` £${numeral(this.props.amount / 100).format('0,0.00')}`}
 										</span>
 									</h3>
 								</div>
@@ -168,23 +113,12 @@ class RecordListItem extends Component
 					(this.state.expand || this.state.stayExpanded) && (
 						<div>
 							<div className='list-item__row'>
-								<div className='list-item__expanded-buttons'>
-									{
-										this.props.recordType === 'DEBT' && (
-											<button
-												className='button--secondary'
-												onClick={this.onDebtClick}
-											>
-												Make Debt Payment
-											</button>
-										)
-									}
-									
+								<div className='list-item__expanded-buttons'>						
 									<Link 
 										to={`/edit-record/${this.props.id}`} 
 										className='button'
 									>
-										Edit Full Record
+										Edit Record
 									</Link>
 								</div>
 							</div>
