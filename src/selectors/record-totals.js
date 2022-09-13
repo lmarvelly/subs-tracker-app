8a9bce45ecff9c257ede3bf0169751430d2e258a
Luke Marvelly <lmarvelly@hotmail.com>
2022-09-13 17:03:06 +0100

    Updated to have a more sophisticated method of working out Outstanding Debt

--- src/selectors/record-totals.js
+++ src/selectors/record-totals.js
@@ -2,20 +2,69 @@ export default ( records ) =>
 {
 	let totalIncome = 0;
 	let totalDebt = 0;
-	records.map( (record) =>
+
+	const playerTotals = [];
+
+	records.map( record => 
 	{
-		if ( typeof record.amount === 'number' ) // if amount is a number
+		// Checking if players Uuid has been added to the list. -1 is returned if not
+		const index = playerTotals.findIndex( player => player.playerId === record.playerUuid );
+
+		if ( typeof record.amount === 'number' )
 		{
-			totalIncome += record.amount;
+			if (record.recordType === 'PAYMENT')
+			{
+				totalIncome += record.amount;
+				if( index === -1 )
+				{
+					playerTotals.push( { playerId: record.playerUuid, balance: record.amount } );
+				}
+				else
+				{
+					playerTotals[index].balance += record.amount; 
+				}
+			}
+			else if (record.recordType === 'DEBT') 
+			{
+				if( index === -1 )
+				{
+					playerTotals.push( { playerId: record.playerUuid, balance: -record.amount } );
+				}
+				else
+				{
+					playerTotals[index].balance -= record.amount; 
+				}
+			}
+			else if(record.recordType === 'SESSION')
+			{
+				const amount = record.amount;
+
+				record.playerList.forEach( listedPlayer =>
+				{
+					// Converting discount into percentage to be paid
+					const percentageToPay = 100 - listedPlayer.discount;
+
+					const index = playerTotals.findIndex( player => player.playerId === listedPlayer.playerUuid );
+					
+					if( index === -1 )
+					{
+						playerTotals.push( { playerId: listedPlayer.playerUuid, balance: -(record.amount / 100 * percentageToPay) } );
+					}
+					else
+					{
+						playerTotals[index].balance -= (record.amount / 100 * percentageToPay); 
+					}
+				});
+			}
 		}
-		if ( typeof record.amountPaid === 'number' ) // if amount is a number
+	});
+
+	// Working out the total outstanding debt
+	playerTotals.forEach(listedPlayer =>
+	{
+		if(listedPlayer.balance < 0)
 		{
-			totalIncome += record.amountPaid;
-			totalDebt -= record.amountPaid;
-		}
-		if ( typeof record.amountOwed === 'number' )
-		{
-			totalDebt += record.amountOwed;
+			totalDebt -= listedPlayer.balance
 		}
 	});
 	
