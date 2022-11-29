c5bd839c3ea5120d8fba4d5b6b90a40b7a1da7ed
Luke Marvelly <lmarvelly@hotmail.com>
2022-11-29 14:10:17 +0000

    Refactoring component to functional component
    Needed to refactor because componentWillReceiveProps was not working and is now depreciated

--- src/components/AddSessionPage.js
+++ src/components/AddSessionPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import SessionForm from './SessionForm';
@@ -7,73 +7,67 @@ import getVisibleSeasons from '../selectors/seasons';
 import { startAddSession } from '../actions/sessions';
 import { startAddSessionName } from '../actions/sessionNames';
 
-export class AddSessionPage extends Component
+export const AddSessionPage = ( props ) =>
 {
-	constructor( props )
+	useEffect(() =>
 	{
-		super( props );
+		const members = props.members ? props.members : [];
+		const seasons = props.seasons ? props.seasons : [];
 
-		// TODO: Sort Season Ascending order
-		this.state =
-		{
-			members: this.props.members ? this.props.members : [],
-			seasons: this.props.seasons ? this.props.seasons : []
-		}
-	};
+		doSeasonsAndMembersExist( seasons, members )
+	}, []);
 
-	componentWillReceiveProps() {
-		const doMembersExist = this.props.members.length > 0;
-		const doSeasonsExist = this.props.seasons.length > 0;
+	const doSeasonsAndMembersExist = ( seasons, members ) =>
+	{
+		const doMembersExist = members.length > 0;
+			const doSeasonsExist = seasons.length > 0;
 
-		if (!doMembersExist || !doSeasonsExist) {
-			const message = () => {
-				if (!doMembersExist && !doSeasonsExist) {
-					return 'There are no existing Seasons or Members. Please create both before creating any records.';
+			if (!doMembersExist || !doSeasonsExist) {
+				const message = () => {
+					if (!doMembersExist && !doSeasonsExist) {
+						return 'There are no existing Seasons or Members. Please create both before creating any records.';
+					}
+					else if (!doMembersExist) {
+						return 'There are no existing Members. Please create a member before creating any records.';
+					}
+					else if (!doSeasonsExist) {
+						return 'There are no existing Seasons. Please create one before creating any records.';
+					}
 				}
-				else if (!doMembersExist) {
-					return 'There are no existing Members. Please create a member before creating any records.';
-				}
-				else if (!doSeasonsExist) {
-					return 'There are no existing Seasons/categories. Please create one before creating any records.';
-				}
+				alert(message());
+				this.props.history.push('/'); // return to dashboard
 			}
-			alert(message());
-			this.props.history.push('/'); // return to dashboard
-		}
+	}
+
+	const onSubmit = ( session ) =>
+	{
+		props.startAddSession(session);
+		props.history.push('/');
 	};
 
-	onSubmit = ( session ) => 
-	{
-		this.props.startAddSession(session);
-		this.props.history.push('/');
-	};
-
-	render()
-	{
-		return (
-			<div>
-				<div className='page-header'>
-					<div className='content-container'>
-						<div className='page-header__content-column'>
-							<div>
-								<h1 className='page-header__title'>Add Session Page</h1>
-								<span>Add a new Group Session</span>
-							</div>
+	return(
+		<div>
+			<div className='page-header'>
+				<div className='content-container'>
+					<div className='page-header__content-column'>
+						<div>
+							<h1 className='page-header__title'>Add Session Page</h1>
+							<span>Add a new Group Session</span>
 						</div>
 					</div>
 				</div>
-				<div className='content-container'>
-					<SessionForm
-						members={ this.props.members }
-						seasons={ this.props.seasons }
-						sessionNames={ this.props.sessionNames }
-						onSubmit={ this.onSubmit }
-						addSessionName={ this.props.addSessionName }
-					/>
-				</div>
 			</div>
-		);
-	}
+			<div className='content-container'>
+				<SessionForm
+					members={ props.members }
+					seasons={ props.seasons }
+					sessionNames={ props.sessionNames }
+					onSubmit={ onSubmit }
+					addSessionName={ props.addSessionName }
+				/>
+			</div>
+		</div>
+	);
 }
 
 const mapStateToProps = ( state, props ) =>
