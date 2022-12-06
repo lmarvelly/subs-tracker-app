74be50fbbb4361bfad2f012b7d3f4ea0043ebc94
Luke Marvelly <lmarvelly@hotmail.com>
2022-12-06 12:17:39 +0000

    Adding test case

--- src/tests/functions/recordTotals.test.js
+++ src/tests/functions/recordTotals.test.js
@@ -3,14 +3,14 @@ import { getMemberTotals, getAttendenceTotals, getSeasonTotals } from '../../fun
 import { records, sessions, members } from '../fixtures/fixures'; 
 
 test('Should get zero for both amount paid and debts', () => 
-{ 
+{
 	const totals = getMemberTotals( [] );
 	expect(totals.totalPaid).toBe(0);
 	expect(totals.totalDebt).toBe(0);
 });
 
 test('Should get the right amounts for One Payment', () => 
-{ 
+{
 	const totals = getMemberTotals( [records[1]] );
 
 	expect(totals.totalPaid).toBe(400);
@@ -18,21 +18,21 @@ test('Should get the right amounts for One Payment', () =>
 });
 
 test('Should get the right amounts for one debt', () => 
-{ 
+{
 	const totals = getMemberTotals( [records[0]] );
 	expect(totals.totalPaid).toBe(0);
 	expect(totals.totalDebt).toBe(400);
 });
 
 test('Should get the right amounts when Amounts Paid is more than Debts', () => 
-{ 
+{
 	const totals = getMemberTotals( records );
 	expect(totals.totalPaid).toBe(9400);
 	expect(totals.totalDebt).toBe(0);
 });
 
 test('Should get the right amounts when Debt is more than Amounts Paid', () => 
-{ 
+{
 	const currentRecords = [ records[0], records[2], records[4] ]
 	const totals = getMemberTotals( currentRecords );
 	expect(totals.totalPaid).toBe(500);
@@ -79,3 +79,13 @@ test('should calculate the right amount for several Sessions, WITHOUT discount, 
  	expect(totals.totalDebt).toBe(1300);
 });
 
+test('should calculate the right amount for Sessions, Payments and Debts for Single Player', () => 
+{
+	// Merging all records together
+	const memberRecords = [records[1], records[4], records[5]]
+	const allRecords = memberRecords.concat(sessions);
+	const totals = getMemberTotals( allRecords, members[1].playerUuid );
+
+	expect(totals.totalPaid).toBe(3900);
+ 	expect(totals.totalDebt).toBe(4900);
+});
