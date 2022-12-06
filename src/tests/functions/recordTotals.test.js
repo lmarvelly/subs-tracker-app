77e920c2b58ac49b1306c103de1565f616e311de
Luke Marvelly <lmarvelly@hotmail.com>
2022-12-06 11:52:04 +0000

    Update test cases

--- src/tests/functions/recordTotals.test.js
+++ src/tests/functions/recordTotals.test.js
@@ -1,31 +1,57 @@
 // TDD is simular to what I have been doing except you'd write the tests first
-import recordTotals from '../../functions/recordTotals';
-import { records, members, seasons, } from '../fixtures/fixures'; 
+import { getMemberTotals, getAttendenceTotals, getSeasonTotals } from '../../functions/recordTotals';
+import { records, sessions, members } from '../fixtures/fixures'; 
 
 test('Should get zero for both amount paid and debts', () => 
 { 
-	const totals = recordTotals( [] );
-	expect(totals.totalIncome).toBe(0);
+	const totals = getMemberTotals( [] );
+	expect(totals.totalPaid).toBe(0);
 	expect(totals.totalDebt).toBe(0);
 });
 
-test('Should get the right amounts for one amount paid', () => 
+test('Should get the right amounts for One Payment', () => 
 { 
-	const totals = recordTotals( [records[1]] );
-	expect(totals.totalIncome).toBe(400);
+	const totals = getMemberTotals( [records[1]] );
+
+	expect(totals.totalPaid).toBe(400);
 	expect(totals.totalDebt).toBe(0);
 });
 
 test('Should get the right amounts for one debt', () => 
 { 
-	const totals = recordTotals( [records[0]] );
-	expect(totals.totalIncome).toBe(0);
+	const totals = getMemberTotals( [records[0]] );
+	expect(totals.totalPaid).toBe(0);
 	expect(totals.totalDebt).toBe(400);
 });
 
-test('Should get the right amounts for both amount paid and debts', () => 
+test('Should get the right amounts when Amounts Paid is more than Debts', () => 
 { 
-	const totals = recordTotals( records );
-	expect(totals.totalIncome).toBe(9400);
-	expect(totals.totalDebt).toBe(4000);
+	const totals = getMemberTotals( records );
+	expect(totals.totalPaid).toBe(9400);
+	expect(totals.totalDebt).toBe(0);
 });
+
+test('Should get the right amounts when Debt is more than Amounts Paid', () => 
+{ 
+	const currentRecords = [ records[0], records[2], records[4] ]
+	const totals = getMemberTotals( currentRecords );
+	expect(totals.totalPaid).toBe(500);
+	expect(totals.totalDebt).toBe(7400);
+});
+
+test('should calculate the right amount, WITHOUT discount, for a Single Session for a member', () => 
+{
+	const totals = getMemberTotals( [sessions[0]], members[0].playerUuid );
+
+	expect(totals.totalPaid).toBe(0);
+ 	expect(totals.totalDebt).toBe(400);
+});
+
+test('should calculate the right amount, WITH discount, for a Single Session for a member', () => 
+{
+	const totals = getMemberTotals( [sessions[0]], members[3].playerUuid );
+
+	expect(totals.totalPaid).toBe(0);
+ 	expect(totals.totalDebt).toBe(200);
+});
+
