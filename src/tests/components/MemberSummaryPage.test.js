57c7c3e05ba6f34dcd631d8e22608ccfd12de9df
Luke Marvelly <lmarvelly@hotmail.com>
2022-12-13 15:23:01 +0000

    Added test Cases

--- src/tests/components/MemberSummaryPage.test.js
+++ src/tests/components/MemberSummaryPage.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { MemberSummaryPage } from '../../components/MemberSummaryPage'
+import { MemberSummaryPage } from '../../components/MemberSummaryPage';
+import { members, records, seasons, sessions } from '../fixtures/fixures';
+import { getMemberTotals, getAttendenceTotals, getSeasonTotals } from '../../functions/recordTotals';
 
 test('should render empty MemberSummaryPage component', () =>
 {
@@ -16,3 +18,40 @@ test('should render empty MemberSummaryPage component', () =>
 
 	expect( emptyWrapper ).toMatchSnapshot();
 });
+
+test('should render MemberSummaryPage component with seasons and records for second member in members array', () =>
+{
+	const member = members[1].playerUuid
+	const allRecords = records.concat(sessions)
+	const recordTotals = getMemberTotals(allRecords, member);
+
+	const recordsWrapper = shallow(
+		<MemberSummaryPage
+			memberFilter={member}
+			recordTotals = {recordTotals}
+			records = {allRecords}
+		/>
+	);
+
+	expect( recordTotals ).toEqual({totalPaid: 9400, totalDebt: 300})
+	expect( recordsWrapper ).toMatchSnapshot();
+});
+
+test('should render MemberSummaryPage component with seasons and records for third member in members array', () =>
+{
+	const member = members[2].playerUuid
+	const allRecords = records.concat(sessions)
+	const recordTotals = getMemberTotals(allRecords, member);
+
+	const recordsWrapper = shallow(
+		<MemberSummaryPage
+			memberFilter={member}
+			recordTotals = {recordTotals}
+			records = {allRecords}
+		/>
+	);
+
+	expect( recordTotals ).toEqual({totalPaid: 9400, totalDebt: 0})
+	expect( recordsWrapper ).toMatchSnapshot();
+});
+
