911ec9f5ec37c7cb47545f7881f3f7ea5da3800c
Luke Marvelly <lmarvelly@hotmail.com>
2022-12-13 12:56:51 +0000

    Adding test cases

--- src/tests/components/MemberSummaryFilters.test.js
+++ src/tests/components/MemberSummaryFilters.test.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { MemberSummaryFilters } from '../../components/MemberSummaryFilters';
-import { members } from '../fixtures/fixures';
+import { members, seasons } from '../fixtures/fixures';
 import { defaultFilters, altFilters1, altFilters2, altFilters3 } from '../fixtures/filters';
 
 let defaultWrapper,
+	memberWrapper,
+	seasonWrapper,
+	memberAndSeasonWrapper,
 	resetRecordFilters,
+	setMemberUuidFilter,
 	setSeasonFilter, 
 	startSetRecords,
 	startSetSessions;
@@ -13,6 +17,7 @@ let defaultWrapper,
 beforeEach( () =>
 {
 	resetRecordFilters = jest.fn();
+	setMemberUuidFilter = jest.fn();
 	setSeasonFilter = jest.fn();
 	startSetRecords = jest.fn();
 	startSetSessions = jest.fn();
@@ -20,6 +25,7 @@ beforeEach( () =>
 	defaultWrapper = shallow(
 		<MemberSummaryFilters
 			resetRecordFilters={resetRecordFilters}
+			setMemberUuidFilter={setMemberUuidFilter}
 			setSeasonFilter={setSeasonFilter}
 			startSetRecords={startSetRecords}
 			startSetSessions={startSetSessions}
@@ -29,6 +35,48 @@ beforeEach( () =>
 			seasons={[]}
 		/>
 	);
+
+	memberWrapper = shallow(
+		<MemberSummaryFilters
+			resetRecordFilters={resetRecordFilters}
+			setMemberUuidFilter={setMemberUuidFilter}
+			setSeasonFilter={setSeasonFilter}
+			startSetRecords={startSetRecords}
+			startSetSessions={startSetSessions}
+
+			recordFilters={defaultFilters}
+			members={members}
+			seasons={[]}
+		/>
+	);
+
+	seasonWrapper = shallow(
+		<MemberSummaryFilters
+			resetRecordFilters={resetRecordFilters}
+			setMemberUuidFilter={setMemberUuidFilter}
+			setSeasonFilter={setSeasonFilter}
+			startSetRecords={startSetRecords}
+			startSetSessions={startSetSessions}
+
+			recordFilters={defaultFilters}
+			members={[]}
+			seasons={seasons}
+		/>
+	);
+
+	memberAndSeasonWrapper = shallow(
+		<MemberSummaryFilters
+			resetRecordFilters={resetRecordFilters}
+			setMemberUuidFilter={setMemberUuidFilter}
+			setSeasonFilter={setSeasonFilter}
+			startSetRecords={startSetRecords}
+			startSetSessions={startSetSessions}
+
+			recordFilters={defaultFilters}
+			members={members}
+			seasons={seasons}
+		/>
+	);
 });
 
 test('should Render MemberSummaryFilters Component correctly without any data', () => 
@@ -36,6 +84,40 @@ test('should Render MemberSummaryFilters Component correctly without any data', 
 	expect(defaultWrapper).toMatchSnapshot();
 
 	expect(resetRecordFilters).toHaveBeenCalled();
+	expect(setMemberUuidFilter).toHaveBeenCalled();
+	expect(setSeasonFilter).toHaveBeenCalled();
+	expect(startSetRecords).toHaveBeenCalled();
+	expect(startSetSessions).toHaveBeenCalled();
+});
+
+test('should Render MemberSummaryFilters Component correctly with Member data', () => 
+{
+	expect(memberWrapper).toMatchSnapshot();
+
+	expect(resetRecordFilters).toHaveBeenCalled();
+	expect(setMemberUuidFilter).toHaveBeenCalled();
+	expect(setSeasonFilter).toHaveBeenCalled();
+	expect(startSetRecords).toHaveBeenCalled();
+	expect(startSetSessions).toHaveBeenCalled();
+});
+
+test('should Render MemberSummaryFilters Component correctly with Season data', () => 
+{
+	expect(seasonWrapper).toMatchSnapshot();
+
+	expect(resetRecordFilters).toHaveBeenCalled();
+	expect(setMemberUuidFilter).toHaveBeenCalled();
+	expect(setSeasonFilter).toHaveBeenCalled();
+	expect(startSetRecords).toHaveBeenCalled();
+	expect(startSetSessions).toHaveBeenCalled();
+});
+
+test('should Render MemberSummaryFilters Component correctly with Member and Season data', () => 
+{
+	expect(memberAndSeasonWrapper).toMatchSnapshot();
+
+	expect(resetRecordFilters).toHaveBeenCalled();
+	expect(setMemberUuidFilter).toHaveBeenCalled();
 	expect(setSeasonFilter).toHaveBeenCalled();
 	expect(startSetRecords).toHaveBeenCalled();
 	expect(startSetSessions).toHaveBeenCalled();
