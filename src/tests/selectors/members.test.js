56fdbe005dbb26c12460a87b3ccb82f05a08e0e2
Luke Marvelly <lmarvelly@hotmail.com>
2022-12-26 14:58:42 +0000

    Updating tests new Extra Member

--- src/tests/selectors/members.test.js
+++ src/tests/selectors/members.test.js
@@ -10,7 +10,7 @@ test('members should be ascending in alphabetical order', () =>
 
 	const result = selectMembers( members, filters );
 
-	expect( result ).toEqual([members[1], members[2], members[3], members[0]]);
+	expect( result ).toEqual([members[1], members[2], members[3], members[0], members[4]]);
 });
 
 test('members should be in descending alphabetical order', () => 
@@ -22,7 +22,7 @@ test('members should be in descending alphabetical order', () =>
 
 	const result = selectMembers( members, filters );
 
-	expect( result ).toEqual([members[0], members[3], members[2], members[1]]);
+	expect( result ).toEqual([ members[4], members[0], members[3], members[2], members[1] ]);
 });
 
 test('should filter by text value and be in alphabetical order', () => 
@@ -48,5 +48,5 @@ test('should filter by more advanced text value and be in alphabetical order', (
 
 	const result = selectMembers( members, filters );
 
-	expect( result ).toEqual([members[2], members[3]]);
+	expect( result ).toEqual([members[2], members[3], members[4],]);
 });
