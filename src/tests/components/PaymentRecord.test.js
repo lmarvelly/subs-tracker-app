45d710dce29d22d77003089732fa1819b889f470
Luke Marvelly <lmarvelly@hotmail.com>
2023-01-01 16:10:05 +0000

    Updating test
    Test not rendering properly with shallow render. So it's been changed to a full mount render.

--- src/tests/components/PaymentRecord.test.js
+++ src/tests/components/PaymentRecord.test.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 // Importing the named export. To dynamically pass in props and make sure it's rendering correctly.
 import { PaymentRecord } from '../../components/PaymentRecord';
 import { records, members, seasons, sessions } from '../fixtures/fixures';
+import { defaultFilters } from '../fixtures/filters';
 
 const allRecords = records.concat(sessions);
 
+const startSetRecords = jest.fn();
+const startSetSessions = jest.fn();
+
 test('should render PaymentRecord with Payments/Debts', () => 
 {
-	const wrapper = shallow( 
+	const wrapper = mount( 
 		<PaymentRecord
-			paymentRecord={records} 
-			members={members} 
-			seasons={seasons} 
-			sessions={sessions} 
+			startSetRecords={startSetRecords}
+			startSetSessions={startSetSessions}
+
+			paymentRecord={records}
+			members={members}
+			seasons={seasons}
 			allRecords={allRecords} 
+
+			recordFilters={defaultFilters}
 		/>
 	);
 
